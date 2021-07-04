f68da0ab725bdf8b89542cdb368e8306fc9929d3
Christian Butcher <chrisb2244@gmail.com>
2021-07-04 16:15:38 +0900

    Add 'upcoming/live/finished' values for the Countdown.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,13 @@ import { Countdown } from './Components/Countdown';
 import './App.css';
 
 function App() {
-  const eventTime = new Date(2021, 10, 15);
-  eventTime.setUTCHours(12, 0, 0);
+  const eventStart = new Date(2021, 10, 15);
+  eventStart.setUTCHours(12, 0, 0);
+  const eventEnd = new Date(eventStart.getTime() + (24*3600000));
   return (
     <div className="App">
       <Header/>
-      <Countdown target={eventTime}/>
+      <Countdown event_start={eventStart} event_end={eventEnd}/>
     </div>
   );
 }
--- frontend/src/Components/Countdown.tsx
+++ frontend/src/Components/Countdown.tsx
@@ -1,52 +1,97 @@
 import { useEffect, useState } from "react";
 
 export type CountdownProps = {
-    target: Date
+  event_start: Date
+  event_end: Date
 }
 
 type TimerValues = {
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
 }
 
+type EventStatus = "Upcoming" | "Live" | "Finished";
+
 export function Countdown(props: CountdownProps) {
-    const [timerVals, setTimerVals] = useState<TimerValues>({days:0,hours:0,minutes:0,seconds:0});
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const tVals = calcTimeValues(props.target);
-            setTimerVals(tVals);
-        }, 1000);
-        return () => {
-            clearInterval(interval)
-        };
-    }, [props.target, setTimerVals]);
+  const [timerVals, setTimerVals] = useState<TimerValues>({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+  const [status, setStatus] = useState<EventStatus>();
 
-    function calcTimeValues(target: Date): TimerValues {
-        const now = new Date().getTime();
-        let remaining = (target.getTime() - now) / 1000;
-        const days = Math.floor(remaining/(3600*24));
-        remaining -= (days * 3600 * 24);
-        const hours = Math.floor(remaining/3600);
-        remaining -= (hours * 3600)
-        const minutes = Math.floor(remaining/60);
-        remaining -= (minutes * 60)
-        const seconds = Math.floor(remaining);
-        return {
-            days: days,
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds,
-        };
-    }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      let distance = (props.event_start.getTime() - now);
+      if (distance > 0) {
+        // Event hasn't started yet
+        const tVals = calcTimeValues(distance);
+        setTimerVals(tVals);
+        setStatus("Upcoming");
+      } else {
+        distance = (props.event_end.getTime() - now);
+        if (distance > 0) {
+          // Event is ongoing
+          const tVals = calcTimeValues(distance);
+          setTimerVals(tVals);
+          setStatus("Live");
+        } else {
+          // Event has finished    
+          setStatus("Finished")
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(interval)
+    };
+  }, [props.event_start, props.event_end, setTimerVals]);
 
-    return (
+  function calcTimeValues(distanceInMilliseconds: number): TimerValues {
+    let remaining = distanceInMilliseconds / 1000;
+    const days = Math.floor(remaining / (3600 * 24));
+    remaining -= (days * 3600 * 24);
+    const hours = Math.floor(remaining / 3600);
+    remaining -= (hours * 3600)
+    const minutes = Math.floor(remaining / 60);
+    remaining -= (minutes * 60)
+    const seconds = Math.floor(remaining);
+    return {
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  let display = <></>
+  switch(status) {
+    case "Upcoming":
+      display = (
         <div>
-            <span>{timerVals.days} days, </span>
-            <span>{timerVals.hours} hours, </span>
-            <span>{timerVals.minutes} minutes, </span>
-            <span>{timerVals.seconds} seconds</span>
+          <span>Event will start in: </span>
+          <span>{timerVals.days} days, </span>
+          <span>{timerVals.hours} hours, </span>
+          <span>{timerVals.minutes} minutes, </span>
+          <span>{timerVals.seconds} seconds.</span>
         </div>
-    )
+      )
+      break;
+    case "Live":
+      display = (
+        <div>
+          <span>Event is live for the next: </span>
+          <span>{timerVals.days} days, </span>
+          <span>{timerVals.hours} hours, </span>
+          <span>{timerVals.minutes} minutes, </span>
+          <span>{timerVals.seconds} seconds.</span>
+        </div>
+      )
+      break;
+    case "Finished":
+      display = (
+        <div>
+          <span>This event has finished.</span>
+        </div>
+      )
+  }
+  return display;
 }
