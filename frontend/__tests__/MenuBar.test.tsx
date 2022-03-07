f9b9f68d6569e64c9802b96682837a34d67f8eff
Christian Butcher <chrisb2244@gmail.com>
2022-03-07 14:30:17 +0900

    Refactor MenuBar tests to reduce duplication.

--- frontend/__tests__/MenuBar.test.tsx
+++ frontend/__tests__/MenuBar.test.tsx
@@ -2,22 +2,18 @@ import { render, screen, within } from '@testing-library/react'
 import { MenuBar } from '@/Components/MenuBar'
 
 describe('MenuBar', () => {
+  render(<MenuBar />)
+  const menu = screen.getByRole('menu')
   it('has the menu role', () => {
-    render(<MenuBar />)
-    const menu = screen.getByRole('menu')
     expect(menu).toBeDefined()
   })
 
   it('has menuitems', () => {
-    render(<MenuBar />)
-    const menu = screen.getByRole('menu')
     const items = within(menu).getAllByRole('menuitem')
     expect(items.length).toBeGreaterThan(0)
   })
 
   it('has items with same font styling', () => {
-    render(<MenuBar />)
-    const menu = screen.getByRole('menu')
     const items = within(menu).getAllByRole('menuitem')
     const { areSameFont } = items.reduce((prev: {areSameFont: boolean, font: string | null}, current) => {
       const className = current.className
