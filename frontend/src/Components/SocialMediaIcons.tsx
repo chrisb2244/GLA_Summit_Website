47fd92f4bcf66c9c1ea296cd0a8b0704e51d7010
Christian Butcher <chrisb2244@gmail.com>
2022-03-08 14:25:41 +0900

    Add the correct icons and links for the SocialMediaIcons component.

--- frontend/src/Components/SocialMediaIcons.tsx
+++ frontend/src/Components/SocialMediaIcons.tsx
@@ -1,18 +1,25 @@
-import { Box } from "@mui/material";
+import { Box } from '@mui/material'
+import { Twitter, Facebook, YouTube, Instagram } from '@mui/icons-material'
 
 export const SocialMediaIcons: React.FC = (props) => {
   return (
     <Box
-      role="grid"
-      aria-label="Social Media Links"
-      sx={{ "& a": { px: 0.4, "& img": { width: "20px" } } }}
+      role='grid'
+      aria-label='Social Media Links'
+      sx={{ '& a': { px: 0.4 }, '.MuiSvgIcon-root': { color: (t) => t.palette.primary.contrastText, fontSize: '18px' } }}
     >
-      <a href="https://abc.com">
-        <img src="media/GLA-logo.svg" />
+      <a aria-label='Twitter link' href='https://twitter.com/glasummit'>
+        <Twitter />
       </a>
-      <a title="blah" href="https://def.com">
-        <img src="media/GLA-logo.svg" />
+      <a aria-label='FaceBook link' href='https://www.facebook.com/GLASummit/'>
+        <Facebook />
+      </a>
+      <a aria-label='YouTube link' href='https://www.youtube.com/channel/UCtQvJ5rMGYoq8n7ExvITVNA'>
+        <YouTube />
+      </a>
+      <a aria-label='Instagram link' href='https://www.instagram.com/glasummit/'>
+        <Instagram />
       </a>
     </Box>
-  );
-};
+  )
+}
