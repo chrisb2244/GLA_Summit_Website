cb564f6bd203cd88831236dee363c95c18637317
Christian Butcher <chrisb2244@gmail.com>
2022-03-08 20:23:00 +0900

    Reorganize MenuBar generation to reduce duplication.

--- frontend/src/Components/MenuBar.tsx
+++ frontend/src/Components/MenuBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import MenuIcon from '@mui/icons-material/Menu'
-import { Typography } from '@mui/material'
+import { Typography, Button, Box } from "@mui/material"
 
 export const MenuBar: React.FC<{ narrowWidth?: boolean }> = (props) => {
   if (props.narrowWidth ?? false) {
@@ -8,28 +8,22 @@ export const MenuBar: React.FC<{ narrowWidth?: boolean }> = (props) => {
   }
 
   return (
-    <ul className='gla-main-menu' role='menu'>
-      <li>
-        <Link href='/'>
-          <Typography variant='button' role='menuitem'>
-            Home
-          </Typography>
-        </Link>
-      </li>
-      <li>
-        <Link href='/our-team'>
-          <Typography variant='button' role='menuitem'>
-            Our Team
-          </Typography>
-        </Link>
-      </li>
-      <li>
-        <Link href='/submit-presentation'>
-          <Typography variant='button' role='menuitem'>
-            Submit a Presentation
-          </Typography>
-        </Link>
-      </li>
-    </ul>
-  )
-}
+    <Box className="gla-main-menu" role="menu" sx={{alignContent: 'center', padding:'5px'}}>
+      {[
+        { title: "Home", link: "/" },
+        { title: "Our Team", link: "/our-team" },
+        { title: "Submit a Presentation", link: "/submit-presentation" },
+      ].map(({ title, link }) => {
+        return (
+          <Button sx={{ color: "white" }} key={title}>
+            <Link href={link}>
+              <Typography variant="button" role="menuitem">
+                {title}
+              </Typography>
+            </Link>
+          </Button>
+        )
+      })}
+    </Box>
+  );
+};
