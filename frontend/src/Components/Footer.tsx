99358294e208d31aff964c62cac8e072c99ec0f2
Christian Butcher <chrisb2244@gmail.com>
2022-03-08 14:24:19 +0900

    Add placeholders with the text for the Privacy policy and the Code of Conduct.

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -4,12 +4,33 @@ import { SocialMediaIcons } from './SocialMediaIcons'
 export const Footer: React.FC = (props) => {
   // The 'Toolbar' component appears to make the flow
   // direction a row, rather than a column otherwise...
+  const FileLink = (props: { link: string, title: string }) => {
+    return (
+      <Typography
+        variant='body2'
+        fontSize='small'
+        sx={{ px: { xs: 0, md: 2 } }}
+      >
+        {props.title}
+      </Typography>
+    )
+  }
 
   return (
     <footer className='App-footer'>
-      <Toolbar sx={{justifyContent: 'space-between', color: (t) => t.palette.primary.contrastText, bgcolor: (theme) => theme.palette.primary.main}}>
-        {/* <object data='media/GLA-logo.svg' className='app-logo' height='100' aria-label='logo' style={{ pointerEvents: 'none' }} /> */}
+      <Toolbar
+        sx={{
+          justifyContent: 'space-between',
+          color: (t) => t.palette.primary.contrastText,
+          bgcolor: (theme) => theme.palette.primary.main,
+          alignItems: 'center'
+        }}
+      >
         <SocialMediaIcons />
+        <Box display='flex' flexDirection={{ xs: 'column', md: 'row' }}>
+          <FileLink title='Code of Conduct' link='' />
+          <FileLink title='Privacy Policy' link='' />
+        </Box>
         <Box display='flex' flexDirection='column'>
           <Typography variant='body2' fontSize='small'>
             {'\u00A9 2022 GLA Organizers'}
