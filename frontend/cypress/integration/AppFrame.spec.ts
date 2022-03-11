b770cca668319e3a9bbe30a06bb08edfd9137ccd
Christian Butcher <chrisb2244@gmail.com>
2022-03-12 02:46:44 +0900

    Add a test via cypress for the height of the banner.

--- frontend/cypress/integration/AppFrame.spec.ts
+++ frontend/cypress/integration/AppFrame.spec.ts
@@ -7,4 +7,10 @@ describe('AppFrame Cypress Tests', () => {
     cy.scrollTo(0,500)
     cy.get('title').should('not.be.visible')
   })
+
+  it.only('has a title with less than 25% height', async () => {
+    cy.visit('http://localhost:3000/lorem-ipsum')
+    const windowHeight = window.outerHeight
+    cy.get('header').invoke('height').should('be.within', 1, windowHeight*0.25)
+  })
 })
