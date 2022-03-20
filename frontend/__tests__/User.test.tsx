180c7b3fada4b8e262047b0a95920a3c473ccde9
Christian Butcher <chrisb2244@gmail.com>
2022-03-20 23:51:56 +0900

    Add examples for the User tests showing mocking the useSession function. These will have to be refactored to test useful behaviour, but show the necessary pattern.

--- frontend/__tests__/User.test.tsx
+++ frontend/__tests__/User.test.tsx
@@ -1,28 +1,50 @@
 import { render } from '@testing-library/react'
 import { User } from '@/Components/User'
 import { Header } from '@/Components/Header'
-import { SessionProvider } from 'next-auth/react'
+import { useSession } from 'next-auth/react'
 import { Session } from 'next-auth'
+
+jest.mock('next-auth/react', () => {
+  return {
+    __esModule: true,
+    ...jest.requireActual('next-auth/react'),
+    useSession: jest.fn()
+  }
+})
+
+type useSessionFn = jest.MockedFunction<typeof useSession>
 
 describe('User', () => {
   it('is included in the menu', () => {
-    const mockSession: Session = {
-      expires: '1',
-      user: { email: 'a', name: 'Test User' }
-    }
+    (useSession as useSessionFn).mockImplementation(() => {
+      return { data: null, status: "unauthenticated"}
+    })
 
-    const header = render(
-      <SessionProvider session={mockSession}>
-        <Header />
-      </SessionProvider>
-    ).container
-    const user = render(
-      <SessionProvider session={mockSession}>
-        <User />
-      </SessionProvider>
-    ).container.innerHTML
+    const header = render(<Header />).container
+    const user = render(<User />).container.innerHTML
     expect(header).toContainHTML(user)
   })
 
-  
+  it('provides a button to sign in if not signed in', () => {
+    (useSession as useSessionFn).mockImplementation(() => {
+      return { data: null, status: "unauthenticated"}
+    })
+
+    const user = render(<User />)
+    expect(user.container).toHaveTextContent('Not signed in')
+  })
+
+  it('logs in with auth data', () => {
+    (useSession as useSessionFn).mockImplementation(() => {
+      return { data: {
+        expires: '1',
+        user: {
+          name: 'Test User'
+        }
+      }, status: "authenticated"}
+    })
+
+    const user = render(<User />)
+    expect(user.container).toHaveTextContent('Signed in as Test User')
+  })
 })
