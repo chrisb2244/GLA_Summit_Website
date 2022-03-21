e12ce4394883431f39e6e2fbb84dce89832c0f29
Christian Butcher <chrisb2244@gmail.com>
2022-03-21 19:16:34 +0900

    Add an email-based login option. This doesn't provide useful information for the Session (i.e. name, etc) but does correctly email and verify the email signin.

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,44 @@
 import NextAuth from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
+import SequelizeAdapter from '@next-auth/sequelize-adapter'
+import { Sequelize } from 'sequelize'
+
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'db',
+  username: 'root',
+  database: 'gla_summit_test',
+  password: process.env.MYSQL_ROOT_PASSWORD
+})
+
+const conn = async () => {
+  await sequelize
+    .authenticate()
+    .then(() => {
+      console.log('Authenticated successfully')
+    })
+    .catch((error) => {
+      console.log('Unable to connect: ', error)
+    })
+}
+conn()
+
+// Calling sync() is not recommended in production
+sequelize.sync()
 
 export default NextAuth({
   providers: [
-  ]
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    }),
+  ],
+  adapter: SequelizeAdapter(sequelize)
 })
