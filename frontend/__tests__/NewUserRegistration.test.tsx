31387c98ad69abda170e7bd08cfd44ce72f5c61f
Christian Butcher <chrisb2244@gmail.com>
2022-03-22 12:43:18 +0900

    Expand tests for the NewUserRegistration.

--- frontend/__tests__/NewUserRegistration.test.tsx
+++ frontend/__tests__/NewUserRegistration.test.tsx
@@ -1,20 +1,33 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { fireEvent } from '@testing-library/react'
 import { NewUserRegistration } from '@/Components/User/NewUserRegistration'
 
-// jest.mock('next-auth/react', () => {
-//   return {
-//     __esModule: true,
-//     ...jest.requireActual('next-auth/react'),
-//     useSession: jest.fn()
-//   }
-// })
-
-// type useSessionFn = jest.MockedFunction<typeof useSession>
-
 describe('NewUserRegistration', () => {
+  const form = <NewUserRegistration open={true} setClosed={()=>{}}/>
+
   it('contains an input for first name', () => {
-    const regForm = render(<NewUserRegistration open={true} setClosed={()=>{}}/>)
-    expect(regForm.getByRole('textbox', {name: 'First name'})).toBeVisible()
+    render(form)
+    expect(screen.getByRole('textbox', {name: /First name/i})).toBeVisible()
   })
 
+  it('contains an input for last name', () => {
+    render(form)
+    expect(screen.getByRole('textbox', {name: /Last name/i})).toBeVisible()
+  })
+
+  it('contains an input for email', () => {
+    render(form)
+    expect(screen.getByRole('textbox', {name: /Email/i})).toBeVisible()
+  })
+
+  it('displays an error for invalid email', () => {
+    render(form)
+    const email = screen.getByRole('textbox', {name: /Email/i})
+    userEvent.type(email, 'blahWithNoAtSymbol')
+    const firstName = screen.getByRole('textbox', {name: /First name/i})
+    // fireEvent.blur(email)
+    userEvent.click(firstName)
+    expect(screen.getByRole('alert', {})).toBeVisible()
+  })
 })
