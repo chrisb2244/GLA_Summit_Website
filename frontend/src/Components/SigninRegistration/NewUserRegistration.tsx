6d70dc0e94caf4bec5e7fc308fa3f459d7453023
Christian Butcher <chrisb2244@gmail.com>
2022-03-29 15:13:58 +0900

    Add instructions to the registration form along with improved spacing/formatting.

--- frontend/src/Components/SigninRegistration/NewUserRegistration.tsx
+++ frontend/src/Components/SigninRegistration/NewUserRegistration.tsx
@@ -8,13 +8,12 @@ import {
   Link
 } from '@mui/material'
 import type { TypographyProps } from '@mui/material'
-import {
-  useForm,
-  FieldErrors,
-  SubmitHandler,
-  Controller,
+import { useForm, Controller } from 'react-hook-form'
+import type {
   Control,
-  UseControllerProps
+  UseControllerProps,
+  SubmitHandler,
+  SubmitErrorHandler
 } from 'react-hook-form'
 
 type UserRegistrationProps = {
@@ -49,7 +48,7 @@ const FormField = (props: {
   name: keyof FormValues
   control: Control<FormValues, any>
   rules?: UseControllerProps<FormValues>['rules']
-}) => {
+}): JSX.Element => {
   const { name, control, rules } = props
 
   return (
@@ -93,52 +92,58 @@ export const NewUserRegistration: React.FC<UserRegistrationProps> = (props) => {
   const { control, handleSubmit, reset } = useForm<FormValues>({
     mode: 'onBlur'
   })
-  const onSubmit: SubmitHandler<FormValues> = (data: FormValues, event) => {
+  const onSubmit: SubmitHandler<FormValues> = (data, event) => {
     console.log(data)
     reset()
     props.setClosed()
   }
-  const onError = (err: FieldErrors<FormValues>) => console.log(err)
+  const onError: SubmitErrorHandler<FormValues> = (err) => console.log(err)
 
   return (
     <Dialog open={props.open} onClose={props.setClosed}>
-      <form
-        onSubmit={handleSubmit(onSubmit, onError)}
-        style={{ display: 'flex', flexDirection: 'column' }}
-      >
-        <Container maxWidth='md' sx={{ pb: 2 }}>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Container maxWidth='md' sx={{ p: 2 }}>
+          <SmallCenteredText sx={{ pb: 2 }}>
+            Please fill out the information below. You will receive an email
+            with a verification link - click the link to automatically sign into
+            the site.
+          </SmallCenteredText>
           <Box mb={2}>
             <FormField
               control={control}
               name='First name'
               rules={{ required: true, maxLength: 80 }}
             />
+          </Box>
+          <Box mb={2}>
             <FormField
               control={control}
               name='Last name'
               rules={{ required: true, maxLength: 100 }}
             />
+          </Box>
+          <Box mb={2}>
             <FormField
               control={control}
               name='Email'
               rules={{ required: true, pattern: /^\S+@\S+$/i }}
             />
-            <Button type='submit' variant='outlined' fullWidth>
-              Register
-            </Button>
-            <SmallCenteredText sx={{ pt: 2 }}>
-              Already registered?{' '}
-              <Link
-                onClick={(ev) => {
-                  ev.preventDefault()
-                  props.switchToSignIn()
-                }}
-                component='button'
-              >
-                Sign In
-              </Link>
-            </SmallCenteredText>
           </Box>
+          <Button type='submit' variant='outlined' fullWidth>
+            Register
+          </Button>
+          <SmallCenteredText sx={{ pt: 2 }}>
+            Already registered?{' '}
+            <Link
+              onClick={(ev) => {
+                ev.preventDefault()
+                props.switchToSignIn()
+              }}
+              component='button'
+            >
+              Sign In
+            </Link>
+          </SmallCenteredText>
         </Container>
       </form>
     </Dialog>
--- frontend/src/Components/SigninRegistration/UserSignIn.tsx
+++ frontend/src/Components/SigninRegistration/UserSignIn.tsx
@@ -57,8 +57,8 @@ export const UserSignIn: React.FC<SignInProps> = (props) => {
   return (
     <Dialog open={props.open} onClose={props.setClosed}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Container maxWidth='md' sx={{ pb: 2 }}>
-          <SmallCenteredText sx={{ p: 2 }}>
+        <Container maxWidth='md' sx={{ p: 2 }}>
+          <SmallCenteredText sx={{ pb: 2 }}>
             In order to sign in, enter the email address you used to register
             for this website. Once completed, you will receive an email with a
             verification link. Open this link to automatically sign into the
