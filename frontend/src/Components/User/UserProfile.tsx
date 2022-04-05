10e5ad08cb6ae541b05302c2fa0cfd7b02f19535
Christian Butcher <chrisb2244@gmail.com>
2022-04-06 03:34:53 +0900

    Add code to set profile values for name.

--- frontend/src/Components/User/UserProfile.tsx
+++ frontend/src/Components/User/UserProfile.tsx
@@ -1,14 +1,44 @@
-import { getProfileInfo } from '@/lib/supabaseClient'
+import { getProfileInfo, supabase } from '@/lib/supabaseClient'
 import type { ProfileModel } from '@/lib/supabaseClient'
-import type { PostgrestError, Session } from '@supabase/supabase-js'
+import type { PostgrestError } from '@supabase/supabase-js'
 import { useEffect, useState } from 'react'
 import { useSession } from '@/lib/sessionContext'
+import { Box, Button, Container, Grid, TextField } from '@mui/material'
 
 export const UserProfile: React.FC = () => {
   const [loading, setLoading] = useState(false)
   const [profileData, setProfileData] = useState<ProfileModel | null>(null)
-
   const session = useSession()
+
+  const updateProfileField = (key: keyof ProfileModel, value: string) => {
+    if (profileData == null) return
+    const newProfileData = { ...profileData, [key]: value }
+    console.log(newProfileData)
+    setProfileData(newProfileData)
+  }
+
+  async function updateProfile() {
+    if (session == null) return
+    try {
+      if (profileData == null) return
+      setLoading(true)
+
+      const { error } = await supabase.from<ProfileModel>('profiles').upsert(
+        { ...profileData, id: session.user?.id },
+        {
+          returning: 'minimal' // Don't return the value after inserting
+        }
+      )
+
+      if (error) {
+        throw error
+      }
+    } catch (error) {
+      alert((error as PostgrestError).message)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
     setLoading(true)
@@ -28,11 +58,43 @@ export const UserProfile: React.FC = () => {
     if (profileData == null) {
       return <p>Loading...</p>
     }
+    console.log(profileData)
     return (
-      <>
-        <div>Name: {profileData.firstname + ' ' + profileData.lastname}</div>
-        <div>Email: {session.user.email}</div>
-      </>
+      <Container>
+        <Box m={2}>
+          <Box p={2}>
+            <TextField
+              fullWidth
+              label='Email'
+              value={session.user.email ?? ''}
+              disabled
+            />
+          </Box>
+          <Grid container p={2}>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label='First Name'
+                value={profileData.firstname ?? ''}
+                onChange={(ev) =>
+                  updateProfileField('firstname', ev.currentTarget.value)
+                }
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label='Last Name'
+                value={profileData.lastname ?? ''}
+                onChange={(ev) =>
+                  updateProfileField('lastname', ev.currentTarget.value)
+                }
+              />
+            </Grid>
+          </Grid>
+          <Button onClick={updateProfile}>Update Profile</Button>
+        </Box>
+      </Container>
     )
   }
 }
