f423c44e7fae538d45d2ac95a066322c2210f3d1
Christian Butcher <chrisb2244@gmail.com>
2022-04-09 21:48:36 +0900

    Add images for several organizers, from previous website.

--- frontend/src/pages/our-team.tsx
+++ frontend/src/pages/our-team.tsx
@@ -1,7 +1,13 @@
 import { Organizer } from '../Components/Organizer'
 import { Grid } from '@mui/material'
-import Image_TM from '@/media/TomMcQuillan.png'
 import { descriptions } from '../organizer-descriptions'
+import Image_OW from '@/media/OliWachno.jpg'
+import Image_TM from '@/media/TomMcQuillan.png'
+// import Image_CB from '@/media/ChristianButcher.png'
+import Image_SS from '@/media/SreejithSreenivasan.jpg'
+import Image_ST from '@/media/SamTaggart.jpg'
+import Image_WR from '@/media/WilliamRichards.jpg'
+import Image_MR from '@/media/MichalRadziwon.jpg'
 
 const OurTeam: React.FC = () => {
   return (
@@ -10,11 +16,15 @@ const OurTeam: React.FC = () => {
         firstName='Oliver'
         lastName='Wachno'
         description={descriptions.OliverWachno}
+        image={Image_OW}
+        imageSide='left'
       />
       <Organizer
         firstName='Christian'
         lastName='Butcher'
         description={descriptions.ChristianButcher}
+        // image={Image_CB}
+        image={Image_TM}
       />
       <Organizer
         firstName='Tom'
@@ -27,21 +37,27 @@ const OurTeam: React.FC = () => {
         firstName='Sreejith'
         lastName='Sreenivasan'
         description={descriptions.SreejithSreenivasan}
+        image={Image_SS}
       />
       <Organizer
         firstName='Sam'
         lastName='Taggart'
         description={descriptions.SamTaggart}
+        image={Image_ST}
+        imageSide='left'
       />
       <Organizer
         firstName='William'
         lastName='Richards'
         description={descriptions.WilliamRichards}
+        image={Image_WR}
       />
       <Organizer
         firstName='Michał'
         lastName='Radziwon'
         description={descriptions.MichalRadziwon}
+        image={Image_MR}
+        imageSide='left'
       />
     </Grid>
   )
