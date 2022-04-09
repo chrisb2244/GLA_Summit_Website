ae673e28459ef16dc41b5083ce7c5a589ac60f59
Christian Butcher <chrisb2244@gmail.com>
2022-04-09 19:50:55 +0900

    Remove the custom headers for next-auth - that package is no longer being used.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,24 +6,6 @@ const nextConfig = {
   reactStrictMode: true
 }
 
-// Return custom headers for NextAuth.
-// See https://github.com/nextauthjs/next-auth/issues/2408
-// and https://nextjs.org/docs/api-reference/next.config.js/headers
-const headers = async () => {
-  return [
-    {
-      source: '/api/auth/:slug',
-      headers: [
-        {
-          key: 'Cache-Control',
-          value: 'no-store, max-age=0'
-        }
-      ]
-    }
-  ]
-}
-
 module.exports = {
-  nextConfig,
-  headers
+  nextConfig
 }
