9ff28e72cb124e1f9d634a66bdf6ce1fddd605c0
Christian Butcher <chrisb2244@gmail.com>
2022-04-17 15:13:54 +0900

    Use the HomePage component in index.tsx rather than duplicating the content.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,21 +1,3 @@
-import React from 'react'
-// import '../index.css'
-
-import { Grid, Typography } from '@mui/material'
-
-export const HomePage: React.FC = () => {
-  return (
-    <Grid container item style={{ margin: '0 auto' }} xs={8} md={6}>
-      <Typography>
-        In collaboration with the Certified LabVIEW Architect community and NI,
-        we&apos;d like to welcome all LabVIEW Architects (certified or
-        self-proclaimed) to join us in our second Global LabVIEW
-        Architects&apos; Summit! This is an exciting opportunity for advanced
-        LabVIEW developers from around the world to network and participate in a
-        more inclusive, all-digital, free event.
-      </Typography>
-    </Grid>
-  )
-}
+import { HomePage } from '@/Components/HomePage'
 
 export default HomePage
