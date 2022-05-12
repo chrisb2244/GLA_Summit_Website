91c3f2f935f54a7ddca9c9eb1b4e8a4503aacb9a
Christian Butcher <chrisb2244@gmail.com>
2022-05-12 19:43:00 +0900

    Center the homepage text, and update the wording from second to third event.

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,5 +1,6 @@
-import { Stack, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import { Countdown } from './Countdown'
+import { StackedBoxes } from './Layout/StackedBoxes'
 
 export const HomePage: React.FC = () => {
   // The month value is 0-based (so 10 -> November)
@@ -8,17 +9,20 @@ export const HomePage: React.FC = () => {
 
   return (
     <>
-      <Stack>
+      <StackedBoxes>
         <Countdown event_start={eventStart} event_end={eventEnd} />
-        <Typography>
+        <Typography textAlign='center'>
           In collaboration with the Certified LabVIEW Architect community and
           NI, we&apos;d like to welcome all LabVIEW Architects (certified or
-          self-proclaimed) to join us in our second Global LabVIEW
-          Architects&apos; Summit! This is an exciting opportunity for advanced
-          LabVIEW developers from around the world to network and participate in
-          a more inclusive, all-digital, free event.
+          self-proclaimed) to join us in our third Global LabVIEW
+          Architects&apos; Summit!
         </Typography>
-      </Stack>
+        <Typography textAlign='center'>
+          This is an exciting opportunity for advanced LabVIEW developers from
+          around the world to network and participate in a more inclusive,
+          all-digital, free event.
+        </Typography>
+      </StackedBoxes>
     </>
   )
 }
