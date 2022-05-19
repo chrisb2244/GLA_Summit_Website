6bd2b69e7ac4b7eacaf32ee5f5a872e4c936d89e
Christian Butcher <chrisb2244@gmail.com>
2022-05-20 03:23:24 +0900

    Allow specifying the vertical spacing with StackedBoxes.

--- frontend/src/Components/Layout/StackedBoxes.tsx
+++ frontend/src/Components/Layout/StackedBoxes.tsx
@@ -1,12 +1,12 @@
 import { Box, Stack } from '@mui/material'
 
-export const StackedBoxes: React.FC = ({ children }) => {
+export const StackedBoxes: React.FC<{stackSpacing?: number}> = ({ children, stackSpacing = 3 }) => {
   const isArray = Array.isArray(children)
   const childArray = isArray ? children : [children]
   return (
     <Stack marginBottom={2} maxWidth='lg'>
       {childArray.map((child, idx) => (
-        <Box marginX={3} key={idx} marginBottom={3}>
+        <Box marginX={3} key={idx} marginBottom={stackSpacing}>
           {child}
         </Box>
       ))}
