a1fa4975ae309660aef9e0380b9074474d958d93
Christian Butcher <chrisb2244@gmail.com>
2022-05-22 16:41:50 +0900

    Add a link to the 'My Presentations' page in the UserMenu. Add a dummy page at the required location.

---
+++ frontend/src/pages/my-presentations.tsx
@@ -0,0 +1,7 @@
+// import { UserPresentations } from '@/Components/User/UserPresentations'
+
+const MyPresentations = (): JSX.Element => {
+  return <p>Your presentations will be listed here soon...</p>
+}
+
+export default MyPresentations
