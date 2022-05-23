26227293f46411dc3ac3ae098159d4afc1f5db35
Christian Butcher <chrisb2244@gmail.com>
2022-05-24 02:27:45 +0900

    Initial plumbing for the draft editor, including confirmation on submission.

--- frontend/src/Components/Form/PresentationEditor.tsx
+++ frontend/src/Components/Form/PresentationEditor.tsx
@@ -1,5 +1,7 @@
 import { PresentationSubmissionsModel } from '@/lib/databaseModels'
 import {
+  Box,
+  Button,
   Card,
   CardContent,
   CardHeader,
@@ -10,6 +12,7 @@ import {
 } from '@mui/material'
 import { useState } from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { ConfirmationPopup } from './ConfirmationPopup'
 
 type PresentationEditorProps = {
   presentation: PresentationSubmissionsModel
@@ -20,28 +23,74 @@ export const PresentationEditor: React.FC<PresentationEditorProps> = ({
 }) => {
   const [expanded, setExpanded] = useState(false)
   const handleExpandClick = () => setExpanded(!expanded)
+  const [showConfirmation, setShowConfirmation] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const saveDraft = () => {
+    console.log('Saving draft')
+    setIsSubmitting(true)
+    setTimeout(() => setIsSubmitting(false), 1000)
+  }
+
+  const submitPresentation = () => {
+    setShowConfirmation(true)
+  }
+
+  const confirmationPopup = (
+    <ConfirmationPopup
+      open={showConfirmation}
+      setClosed={() => setShowConfirmation(false)}
+      onResolve={(confirmed) => {
+        if (confirmed) {
+          console.log('You submitted the presentation')
+          setShowConfirmation(false)
+          setIsSubmitting(true)
+          setTimeout(() => setIsSubmitting(false), 1000)
+        } else {
+          console.log('Cancelled')
+          setShowConfirmation(false)
+        }
+      }}
+    />
+  )
 
   return (
-    <Card>
-      <CardHeader
-        title={presentation.title}
-        action={
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label='show more'
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        }
-      />
-      <Collapse in={expanded} timeout='auto' unmountOnExit>
-        <CardContent>
-          <Typography>Abstract: {presentation.abstract}</Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+    <>
+      <Card>
+        <CardHeader
+          title={presentation.title}
+          action={
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label='show more'
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          }
+        />
+        <Collapse in={expanded} timeout='auto' unmountOnExit>
+          <CardContent>
+            <Typography>Abstract: {presentation.abstract}</Typography>
+            <Box
+              flexDirection={{ xs: 'column', md: 'row' }}
+              display='flex'
+              gap={1}
+              pt={2}
+            >
+              <Button variant='outlined' disabled={isSubmitting} sx={{flexGrow: 1}} onClick={saveDraft}>
+                {isSubmitting ? 'Saving draft...' : 'Save Draft'}
+              </Button>
+              <Button variant='contained' disabled={isSubmitting} sx={{flexGrow: 1}} onClick={submitPresentation}>
+                {isSubmitting ? 'Submitting now...' : 'Submit Presentation'}
+              </Button>
+            </Box>
+          </CardContent>
+        </Collapse>
+      </Card>
+      {confirmationPopup}
+    </>
   )
 }
 
