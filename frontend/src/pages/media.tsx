99b2eab591141b269e9e4babf6efcb89142d608e
Christian Butcher <chrisb2244@gmail.com>
2022-09-15 16:25:13 +0900

    Switch to links for the images with a single example in each case. This will make the page load significantly faster, I expect, and reduces the scrolling size for repeated images (not useful for most users).

--- frontend/src/pages/media.tsx
+++ frontend/src/pages/media.tsx
@@ -13,46 +13,93 @@ import { NextPage } from 'next'
 import NextImage from 'next/image'
 import { Box, Typography } from '@mui/material'
 import type { TypographyProps, BoxProps } from '@mui/material'
-
+import { CopyableTextBox } from '@/Components/CopyableTextBox'
 
 const MediaPage: NextPage = () => {
-  const hostname = typeof window !== 'undefined' ? window.location.protocol + '//' + window.location.host : ''
+  const hostname =
+    typeof window !== 'undefined'
+      ? window.location.protocol + '//' + window.location.host
+      : ''
 
-  const bannerImageArrayAttendee = [FB_Attendee, IN_Attendee, LI_Attendee, TW_Attendee]
-  const bannerImagesAttendee = bannerImageArrayAttendee.map((img, idx) => {
-    return <NextImage key={`bannerimage-attendee-${idx}`} src={img} />
-  })
-  const bannerImageArraySpeaker = [FB_Speaker, IN_Speaker, LI_Speaker, TW_Speaker]
-  const bannerImagesSpeaker = bannerImageArraySpeaker.map((img, idx) => {
-    return <NextImage key={`bannerimage-speaker-${idx}`} src={img} />
-  })
+  const images = [
+    { label: 'Facebook', attendeeImg: FB_Attendee, speakerImg: FB_Speaker },
+    { label: 'Instagram', attendeeImg: IN_Attendee, speakerImg: IN_Speaker },
+    { label: 'LinkedIn', attendeeImg: LI_Attendee, speakerImg: LI_Speaker },
+    { label: 'Twitter', attendeeImg: TW_Attendee, speakerImg: TW_Speaker }
+  ]
 
-  const Subtitle: React.FC<TypographyProps> = ({children, ...other}) => <Typography variant='h4' {...other}>{children}</Typography>;
-  const Paragraph: React.FC<TypographyProps> = ({children, ...other}) => <Typography variant='body1' {...other}>{children}</Typography>;
-  const SidewaysBox: React.FC<BoxProps> = ({children, ...other}) => {
-    return <Box display='flex' flexDirection={{xs: 'column', sm: 'row'}} alignItems='center' {...other}>{children}</Box>
-  }  
+  const bannerImagesAttendee = images
+    .map((elem) => {
+      const img = elem.attendeeImg
+      const url = hostname + img.src
+      return (
+        <a key={`bannerimage-link-attendee-${elem.label}`} href={url}>
+          <Typography>
+            {`Attendee - ${elem.label}: (${img.width}x${img.height} px)`}
+          </Typography>
+        </a>
+      )
+    })
+    .concat(<NextImage key='bannerimage-attendee' src={TW_Attendee} />)
+
+  const bannerImagesSpeaker = images
+    .map((elem) => {
+      const img = elem.speakerImg
+      const url = hostname + img.src
+      return (
+        <a key={`bannerimage-link-speaker-${elem.label}`} href={url}>
+          <Typography>
+            {`Speaker - ${elem.label}: (${img.width}x${img.height} px)`}
+          </Typography>
+        </a>
+      )
+    })
+    .concat(<NextImage key='bannerimage-speaker' src={TW_Speaker} />)
+
+  const Subtitle: React.FC<TypographyProps> = ({ children, ...other }) => (
+    <Typography variant='h4' {...other}>
+      {children}
+    </Typography>
+  )
+  const Paragraph: React.FC<TypographyProps> = ({ children, ...other }) => (
+    <Typography variant='body1' {...other}>
+      {children}
+    </Typography>
+  )
+  const SidewaysBox: React.FC<BoxProps> = ({ children, ...other }) => {
+    return (
+      <Box
+        display='flex'
+        flexDirection={{ xs: 'column', sm: 'row' }}
+        alignItems='center'
+        {...other}
+      >
+        {children}
+      </Box>
+    )
+  }
 
   return (
     <StackedBoxes>
       <SidewaysBox paddingX={4}>
         <NextImage src={JKI_Logo} />
         <Typography marginX={2}>
-          The GLA Summit Organizers would like to thank JKI for providing us with
-          the images and banners available below, along with other graphics
+          The GLA Summit Organizers would like to thank JKI for providing us
+          with the images and banners available below, along with other graphics
           support.
         </Typography>
       </SidewaysBox>
       <Paragraph>
         Please feel free to use the images on this page on your social media or
         website (including your NI forum signature). Links to the images can be
-        found in the &lsquo;href&rsquo; attributes of the HTML samples, or by 
-        right-clicking and choosing an option like &ldquo;Copy image address&rdquo;.
+        found in the &lsquo;href&rsquo; attributes of the HTML samples, or by
+        right-clicking and choosing an option like &ldquo;Copy image
+        address&rdquo;.
       </Paragraph>
       <Paragraph>
         To access your signature on the NI Community pages, go to your community
-        account &ldquo;My Profile&rdquo; settings and then Personal &gt; Personal
-        Information.
+        account &ldquo;My Profile&rdquo; settings and then Personal &gt;
+        Personal Information.
       </Paragraph>
       <Paragraph>
         Examples of HTML that could be copied into the signature line are below:
@@ -60,11 +107,16 @@ const MediaPage: NextPage = () => {
 
       <SidewaysBox>
         <NextImage src={SignatureImage} />
-        <Typography variant='body2' fontFamily='monospace' bgcolor='lightgrey' margin={3} padding={2}>
-          &lt;a href=&ldquo;https://glasummit.org&rdquo;&gt;
-          &lt;img src=&ldquo;{hostname + SignatureImage.src}&rdquo; height=&ldquo;100&rdquo; width=&ldquo;300&rdquo; alt=&ldquo;I&apos;m attending the GLA Summit!&rdquo;&gt;
-          &lt;/a&gt;
-        </Typography>
+        <CopyableTextBox
+          fill='lightgrey'
+          variant='body2'
+          fontFamily='monospace'
+        >
+          &lt;a href=&quot;https://glasummit.org&quot;&gt; &lt;img src=&quot;
+          {hostname + SignatureImage.src}&quot; height=&quot;100&quot;
+          width=&quot;300&quot; alt=&quot;I&apos;m attending the GLA
+          Summit!&quot;&gt; &lt;/a&gt;
+        </CopyableTextBox>
       </SidewaysBox>
 
       <Subtitle>Attendees</Subtitle>
@@ -72,7 +124,6 @@ const MediaPage: NextPage = () => {
 
       <Subtitle>Speakers</Subtitle>
       {bannerImagesSpeaker}
-
     </StackedBoxes>
   )
 }
