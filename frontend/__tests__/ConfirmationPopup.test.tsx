83cb6b81531144fbde88ad4dc82081f418367d0b
Christian Butcher <chrisb2244@gmail.com>
2022-09-28 11:00:09 +0900

    Expand tests for the ConfirmationPopup.

--- frontend/__tests__/ConfirmationPopup.test.tsx
+++ frontend/__tests__/ConfirmationPopup.test.tsx
@@ -3,11 +3,16 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 const onCloseFn = jest.fn(() => {})
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const onResolveFn = jest.fn((value: boolean) => {})
 
 const EmptyConfPopup = <ConfirmationPopup open setClosed={onCloseFn} onResolve={onResolveFn} />
 
 describe('ConfirmationPopup', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
   it('contains two buttons', () => {
     render(EmptyConfPopup)
     expect(screen.getAllByRole('button')).toHaveLength(2)
@@ -32,5 +37,26 @@ describe('ConfirmationPopup', () => {
     const cancelButton = screen.getByRole('button', {name: 'Cancel'})
     userEvent.click(cancelButton)
     expect(onCloseFn).toBeCalledTimes(1)
+  })
+
+  it('calls the setClosed function when resolved true', () => {
+    render(EmptyConfPopup)
+    const submitButton = screen.getByRole('button', {name: 'Confirm'})
+    userEvent.click(submitButton)
+    expect(onCloseFn).toBeCalledTimes(1)
+  })
+
+  it('calls the onResolve function with true when confirmed', () => {
+    render(EmptyConfPopup)
+    const submitButton = screen.getByRole('button', {name: 'Confirm'})
+    userEvent.click(submitButton)
+    expect(onResolveFn).toHaveBeenCalledWith(true)
+  })
+
+  it('calls the onResolve function with false when cancelled', () => {
+    render(EmptyConfPopup)
+    const cancelButton = screen.getByRole('button', {name: 'Cancel'})
+    userEvent.click(cancelButton)
+    return expect(onResolveFn).toHaveBeenCalledWith(false)
   })
 })
