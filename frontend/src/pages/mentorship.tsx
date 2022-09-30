ba9f422bef169343bffac2408a61efcb02c0ca4c
Christian Butcher <chrisb2244@gmail.com>
2022-09-30 17:02:46 +0900

    Add a backend for the mentorship page.

--- frontend/src/pages/mentorship.tsx
+++ frontend/src/pages/mentorship.tsx
@@ -3,15 +3,35 @@ import { useSession } from '@/lib/sessionContext'
 import { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
 import type { PersonProps } from '@/Components/Form/Person'
+import { supabase } from '@/lib/supabaseClient'
+import { myLog } from '@/lib/utils'
 
 const MentorshipPage: NextPage = () => {
   const { user, profile } = useSession()
   const [defaultPerson, setDefaultPerson] = useState<PersonProps | undefined>(
     undefined
   )
+  const [existingRegistrationType, setRegistrationStatus] = useState<RegistrationType | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    const updateRegStatus = async (email: string | undefined) => {
+      if (typeof email === 'undefined') {
+        setLoading(false)
+        return
+      }
+      const { data, error } = await supabase.from('mentoring').select('entry_type').eq('email', email)
+      if (error) {
+        setLoading(false)
+        throw error
+      }
+      if(typeof data != 'undefined' && data.length === 1) {
+        setRegistrationStatus(data[0].entry_type)
+      }
+      setLoading(false)
+    }
     if (user !== null && profile !== null) {
+      setLoading(true)
       const newPerson = {
         firstName: profile?.firstname ?? '',
         lastName: profile?.lastname ?? '',
@@ -19,20 +39,33 @@ const MentorshipPage: NextPage = () => {
       }
       setDefaultPerson(newPerson)
     }
+    updateRegStatus(user?.email)
   }, [profile, user])
 
-  const existingRegistrationType: RegistrationType | null = null
-
   const registrationFn = async (data: RegistrationData) => {
-    console.log(data)
+    const { firstName, lastName, email } = data.person
+    const { error} = await supabase.from('mentoring').insert({
+      entry_type: data.type,
+      firstname: firstName,
+      lastname: lastName,
+      email
+    })
+    if (error) {
+      myLog(error)
+    }
+    
   }
 
   return (
-    <MentoringForm
-      defaultEntry={defaultPerson}
-      registered={existingRegistrationType}
-      registrationFn={registrationFn}
-    />
+    <>
+      {!loading && (
+        <MentoringForm
+          defaultEntry={defaultPerson}
+          registered={existingRegistrationType}
+          registrationFn={registrationFn}
+        />
+      )}
+    </>
   )
 }
 
