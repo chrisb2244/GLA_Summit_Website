7689c3d65af32dd5904d7c3566126bb750e7ec3f
Christian Butcher <chrisb2244@gmail.com>
2022-10-03 16:01:24 +0900

    Adjust method to provide the appropriate dialog within RegistrationPopup.

--- frontend/src/Components/SigninRegistration/RegistrationPopup.tsx
+++ frontend/src/Components/SigninRegistration/RegistrationPopup.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { NewUserRegistration, SignUpFunction } from "./NewUserRegistration";
-import { UserSignIn, SignInFunction } from "./UserSignIn";
+import { useState } from 'react'
+import { NewUserRegistration, SignUpFunction } from './NewUserRegistration'
+import { UserSignIn, SignInFunction } from './UserSignIn'
 
 export type RegistrationProps = {
   open?: boolean
@@ -13,19 +13,33 @@ export type RegistrationProps = {
 export const RegistrationPopup: React.FC<RegistrationProps> = (props) => {
   const {
     open = false,
-    setClosed = () => { return },
+    setClosed = () => {
+      return
+    },
     signUp,
     signIn,
     initialState = 'signup'
   } = props
 
-  const [state, setState] = useState<'signup'|'signin'>(initialState)
-  
+  const [state, setState] = useState<'signup' | 'signin'>(initialState)
+
   // Use the state checks outside of the 'open' property to avoid rendering the unused dialog
-  return (
-    <>
-      {state==='signup' && <NewUserRegistration open={open} setClosed={setClosed} signUp={signUp} switchToSignIn={() => setState('signin')} />}
-      {state==='signin' && <UserSignIn open={open} setClosed={setClosed} signIn={signIn} switchToRegistration={() => setState('signup')} />}
-    </>
-  )
+  const elemToRender =
+    state === 'signup' ? (
+      <NewUserRegistration
+        open={open}
+        setClosed={setClosed}
+        signUp={signUp}
+        switchToSignIn={() => setState('signin')}
+      />
+    ) : (
+      <UserSignIn
+        open={open}
+        setClosed={setClosed}
+        signIn={signIn}
+        switchToRegistration={() => setState('signup')}
+      />
+    )
+
+  return elemToRender
 }
