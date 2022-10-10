d458231b8e2d5285ee03c6369e122c66de5fda2f
Christian Butcher <chrisb2244@gmail.com>
2022-10-10 22:53:37 +0900

    Add tests for initial PoC for e2e testing over email, supabase.

---
+++ frontend/playwright/user-login.spec.ts
@@ -0,0 +1,69 @@
+import { test, expect } from '@playwright/test';
+import { localIP, reqToBody as parseRequestBody } from './utils';
+import http from 'http'
+
+
+const dummyServer = http.createServer()
+test.beforeAll(() => {
+  dummyServer.listen(3001)
+})
+test.afterAll(() => {
+  dummyServer.close()
+})
+
+test('User can log-in', async ({page}) => {
+  const testAddress = 'http://' + localIP + ':3001' + '/testEndpoint'
+  const requests: any[] = [];
+
+  dummyServer.addListener('request', (req, res) => {
+    console.log('Caught with dummyServer')
+    parseRequestBody(req, (body) => {
+      const jbody = JSON.parse(body)
+      requests.push(jbody)
+    })
+    res.writeHead(200)
+    return res.end()
+  })
+
+  await page.route('/api/auth_links/*', (route, request) => {
+    console.log('Routed auth_links via PW')
+    route.continue({
+      headers: {
+        ...request.headers(),
+        // headers are converted to lower case... write in lower case to avoid confusion
+        test_only_headers_blocked: testAddress
+      }
+    })
+  });
+
+  await page.goto('/');
+
+  const registerLoginButton = page.locator('role=button', {hasText: /sign in/i});
+  await expect(registerLoginButton).toBeVisible();
+  
+  registerLoginButton.click();
+  const dialog = page.locator('role=dialog');
+  await expect(dialog).toBeVisible();
+
+  // Should default to register
+  const fnameBox = page.locator('[placeholder="First Name"]');
+  const lnameBox = page.locator('[placeholder="Last Name"]');
+  const emailBox = page.locator('[placeholder="Email"]');
+  await expect(fnameBox).toBeVisible();
+
+  const randomVal = Math.floor(Math.random()*10000).toString();
+  await fnameBox.fill('Test_' + randomVal);
+  await lnameBox.fill('User');
+  await emailBox.fill(`test_${randomVal}.user@glasummit.org`);
+
+  const submitButton = page.locator('role=button', { hasText: "REGISTER" })
+  await expect(submitButton).toBeVisible();
+
+  await submitButton.click()
+  await expect(page.locator('role=dialog', { hasText: /Thank you.*check.*email/i })).toBeVisible()
+  // await page.waitForTimeout(3*1000);
+  expect(requests).toHaveLength(1)
+  const bodyPlain = requests[0]['bodyPlain'] as string
+  expect(bodyPlain).toBeDefined()
+  expect(bodyPlain).toContain('thisistheactionlink')
+});
---
+++ frontend/playwright/utils.ts
@@ -0,0 +1,28 @@
+import { IncomingMessage } from 'http';
+import { networkInterfaces } from 'os';
+
+const results = Object.create(null);
+const nets = networkInterfaces()
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+    const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+      if (net.family === familyV4Value && !net.internal) {
+          if (!results[name]) {
+              results[name] = [];
+          }
+          results[name].push(net.address);
+      }
+  }
+}
+
+export const reqToBody = (req: IncomingMessage, callback: (body: string) => void) => {
+  let body = ''
+  req.on('data', (chunk) => {
+    body += chunk
+  })
+  req.on('end', () => {
+    callback(body)
+  })
+}
+
+export const localIP = results['eth0'][0]
