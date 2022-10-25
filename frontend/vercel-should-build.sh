2095158aab475adba404a915b32944ec68f03350
Christian Butcher <chrisb2244@gmail.com>
2022-10-26 06:39:37 +0900

    Allow this branch to build. Drop this commit on merge.

--- frontend/vercel-should-build.sh
+++ frontend/vercel-should-build.sh
@@ -3,7 +3,7 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 exit 1;
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "maintenancePage" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "maintenancePage" || "$VERCEL_GIT_COMMIT_REF" == "next-13" ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
