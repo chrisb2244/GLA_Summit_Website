e3398bee9b65ce74a14ad377f70b7af06f544d6b
Christian Butcher <chrisb2244@gmail.com>
2022-10-30 20:17:55 +0900

    Update the types in the 'logs' page. Correctly call 'removeChannel(chn)' rather than chn.unsubscribe() to remove a subscription.

--- frontend/src/pages/logs/index.tsx
+++ frontend/src/pages/logs/index.tsx
@@ -1,46 +1,37 @@
-import { NextPage } from "next"
-import { supabase } from "@/lib/supabaseClient"
-import { useEffect, useReducer } from "react"
-import { LogEntry, LogViewer } from "@/Components/LogViewer"
-import { myLog } from "@/lib/utils"
+import { NextPage } from 'next'
+import { supabase } from '@/lib/supabaseClient'
+import { useEffect, useReducer } from 'react'
+import { LogEntry, LogViewer } from '@/Components/LogViewer'
+import { myLog } from '@/lib/utils'
+import { RealtimePostgresChangesPayload } from '@supabase/realtime-js'
+import { Database } from '@/lib/sb_databaseModels'
 
-type UpdatePartialType = {
-  commit_timestamp: string,
-  errors: null | object,
-  new: LogEntry,
-  old: Record<string, never>,
-  eventType: 'INSERT'
-} | {
-  commit_timestamp: string,
-  errors: null | object,
-  new: LogEntry,
-  old: {id: number},
-  eventType: 'UPDATE'
-} | {
-  commit_timestamp: string,
-  errors: null | object,
-  new: Record<string, never>,
-  old: { id: number },
-  eventType: 'DELETE'
-} | {
-  eventType: 'INITIALIZE',
-  data: LogEntry[]
-}
+type DB_SubscriptionEvent = RealtimePostgresChangesPayload<
+  Database['public']['Tables']['log']['Row']
+>
+type SubscriptionEvent =
+  | DB_SubscriptionEvent
+  | {
+      eventType: 'INITIALIZE'
+      data: LogEntry[]
+    }
 
 const LogsPage: NextPage = () => {
   const getInitialLogs = async () => {
     const { data, error } = await supabase.from('log').select('*')
-    if (error) throw error;
+    if (error) throw error
     return data
   }
 
-  
-  const logEntryReducer = (cachedEntries: LogEntry[], payload: UpdatePartialType) => {
-    switch(payload.eventType) {
+  const logEntryReducer = (
+    cachedEntries: LogEntry[],
+    payload: SubscriptionEvent
+  ) => {
+    switch (payload.eventType) {
       case 'INSERT':
         return cachedEntries.concat(payload.new)
       case 'DELETE':
-        return cachedEntries.filter(({id}) => id !== payload.old.id)
+        return cachedEntries.filter(({ id }) => id !== payload.old.id)
       case 'UPDATE':
         return cachedEntries.map((e) => {
           return e.id === payload.new.id ? payload.new : e
@@ -51,9 +42,9 @@ const LogsPage: NextPage = () => {
   }
 
   const [logEntries, updateLogEntries] = useReducer(logEntryReducer, [])
-        
+
   useEffect(() => {
-    getInitialLogs().then(logs => {
+    getInitialLogs().then((logs) => {
       updateLogEntries({
         eventType: 'INITIALIZE',
         data: logs
@@ -63,25 +54,28 @@ const LogsPage: NextPage = () => {
 
   useEffect(() => {
     myLog('adding subscription')
-    const subscription = supabase.channel('public:log')
-    .on('postgres_changes',
-      {
-        event: '*',
-        schema: 'public',
-        table: 'log'
-      },
-      (payload: UpdatePartialType) => { updateLogEntries(payload) }
-    ).subscribe()
+    const subscription = supabase
+      .channel('public:log')
+      .on(
+        'postgres_changes',
+        {
+          event: '*',
+          schema: 'public',
+          table: 'log'
+        },
+        (payload: DB_SubscriptionEvent) => {
+          updateLogEntries(payload)
+        }
+      )
+      .subscribe()
 
     return () => {
       myLog('unsubscribing')
-      subscription.unsubscribe()
+      supabase.removeChannel(subscription)
     }
   }, [])
 
-  return (
-    <LogViewer entries={logEntries} />
-  )
+  return <LogViewer entries={logEntries} />
 }
 
 export default LogsPage
