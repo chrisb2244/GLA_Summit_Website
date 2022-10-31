59398597a6f12b9d32cf357dcf97c1ddb8b638d8
Christian Butcher <chrisb2244@gmail.com>
2022-10-31 23:47:08 +0900

    Move the maintenanceMode redirect to middleware from the next config. (Required for possible turbopack in future).

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,15 +10,5 @@ module.exports = {
   nextConfig,
   images: {
     domains: ['iuqlmccpbxtgcluccazt.supabase.co']
-  },
-  redirects() {
-    const maintenanceModeRedirect = 
-      process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true"
-        ? { source: "/(.+)", destination: '/', permanent: false}
-        : null
-    
-    return [
-      maintenanceModeRedirect,
-    ].filter(Boolean)
   }
 }
