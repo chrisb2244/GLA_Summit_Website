e2864d4d921f2951e0732575cd3f887352f7642c
Christian Butcher <chrisb2244@gmail.com>
2022-10-31 23:43:53 +0900

    Rewrite middleware to use the withMiddlewareAuth helper.

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,54 +1,53 @@
-import { NextRequest, NextResponse } from 'next/server'
-import {
-  createAdminClient,
-  createServerClient_UserRLS
-} from './lib/supabaseClient'
-import { myLog } from './lib/utils'
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+import { withMiddlewareAuth } from '@supabase/auth-helpers-nextjs'
+import { createAdminClient } from './lib/supabaseClient'
 
-export async function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest, event: NextFetchEvent) {
   const targetPath = req.nextUrl.pathname
-  const deniedUrl = req.nextUrl.origin + '/access-denied'
 
-  const res = NextResponse.next()
-  const supabase = createServerClient_UserRLS(req, res)
-  const {
-    data: { user },
-    error
-  } = await supabase.auth.getUser()
+  const pathsToFilter = [
+    '/review-submissions',
+    '/api/presentation_submissions',
+    '/logs'
+  ]
 
-  if (
-    targetPath.startsWith('/review-submissions') ||
-    targetPath.startsWith('/api/presentation_submissions')
-  ) {
-    if (error || user === null) {
-      if (error && error.name !== 'AuthApiError') {
-        myLog({ m: 'User was null, or an error occurred', user, error })
+  if (!pathsToFilter.includes(targetPath)) {
+    return NextResponse.next()
+  }
+  console.log(targetPath)
+  const mw = withMiddlewareAuth({
+    authGuard: {
+      redirectTo: '/access-denied',
+      isPermitted: async (user, supabase) => {
+        if (
+          targetPath.startsWith('/review-submissions') ||
+          targetPath.startsWith('/api/presentation_submissions')
+        ) {
+          if (user === null) {
+            return false
+          }
+
+          const isOrganizer =
+            (await supabase.from('organizers').select('*').eq('id', user.id))
+              .count !== 0
+          return isOrganizer
+        }
+
+        if (targetPath.startsWith('/logs')) {
+          const client = createAdminClient()
+          const { data, error } = await client
+            .from('log_viewers')
+            .select('user_id')
+          if (error || user === null) {
+            return false
+          }
+          const allowedIds: string[] = data.map((value) => value.user_id)
+          return allowedIds.includes(user.id)
+        }
+
+        return true
       }
-      return NextResponse.redirect(deniedUrl)
     }
-
-    const isOrganizer =
-      (await supabase.from('organizers').select('*').eq('id', user.id))
-        .count !== 0
-
-    if (isOrganizer) {
-      return NextResponse.rewrite(req.url)
-    } else {
-      return NextResponse.redirect(deniedUrl)
-    }
-  }
-
-  if (targetPath.startsWith('/logs')) {
-    const client = createAdminClient()
-    const { data, error } = await client.from('log_viewers').select('user_id')
-    if (error || user === null) {
-      return NextResponse.redirect(deniedUrl)
-    }
-    const allowedIds: string[] = data.map((value) => value.user_id)
-    if (allowedIds.includes(user.id)) {
-      return NextResponse.rewrite(req.url)
-    } else {
-      return NextResponse.redirect(deniedUrl)
-    }
-  }
+  })
+  return mw(req, event)
 }
