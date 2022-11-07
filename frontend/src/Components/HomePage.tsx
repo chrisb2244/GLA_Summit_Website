8e9693a156d235af990966447a88331450e86576
Christian Butcher <chrisb2244@gmail.com>
2022-11-08 03:57:33 +0900

    Add corgibytes and heartware logos.

--- frontend/src/Components/HomePage.tsx
+++ frontend/src/Components/HomePage.tsx
@@ -1,9 +1,13 @@
 import { Box, Button, Typography } from '@mui/material'
 import type { TypographyProps } from '@mui/material'
 import { Countdown } from './Countdown'
-import { SponsorBar } from './SponsorBar'
 import { StackedBoxes } from './Layout/StackedBoxes'
 import { Link } from '@/lib/link'
+import NI_Logo from '@/media/NI-Logo.png'
+import GCentral_Logo from '@/media/GCentral-logo-color.svg'
+import CorgiBytes_Logo from '@/media/corgibytes-logo.png'
+import HeartWare_Dev_Logo from '@/media/heartware-dev-logo.png' // #1e143e_bg
+import NextImage, { StaticImageData } from 'next/image'
 // import { SnackbarNotification } from './Utilities/SnackbarNotification'
 
 export const HomePage: React.FC = () => {
@@ -23,56 +27,115 @@ export const HomePage: React.FC = () => {
   //   vertical: 'bottom'
   // }
 
+  const mainBlock = (
+    <StackedBoxes>
+      <P>
+        The GLA Summit Organizers would like to welcome all LabVIEW enthusiasts
+        to join us in our third GLA Summit!
+        <br />
+        The Summit will run between 12:00 UTC, 14<sup>th</sup> and 12:00 UTC, 15
+        <sup>th</sup> November 2022, for a full 24 hours.
+      </P>
+      <P>
+        This is an exciting opportunity for advanced LabVIEW developers and
+        Architects (certified or self-proclaimed) from around the world to
+        network and participate in an inclusive, all-digital, free event.
+      </P>
+      <P>
+        Presenters at the GLA Summit will be eligible to receive 30
+        recertification points for NI LabVIEW certifications.
+        <br />
+        Attendees will also be able to receive 20 points.
+      </P>
+      <Box>
+        <Link href={'https://hopin.com/events/gla-summit-2022'}>
+          <Button fullWidth variant='contained' className='bg-primaryc'>
+            Register for a ticket at Hopin
+          </Button>
+        </Link>
+      </Box>
+    </StackedBoxes>
+  )
+
+  const NI_Block = (
+    <Link href='https://www.ni.com' py={2}>
+      <Box
+        display='flex'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='center'
+        width='fit-content'
+      >
+        <Typography
+          variant='h5'
+          sx={{ fontSize: { md: '2.5rem' }, textAlign: 'center' }}
+          px={2}
+        >
+          Sponsored by NI
+        </Typography>
+        <NextImage src={NI_Logo} width='180px' height='180px' />
+      </Box>
+    </Link>
+  )
+
+  const gcentralBlock = (
+    <Link href={'https://www.gcentral.org/'} py={2}>
+      <Box
+        display='flex'
+        flexDirection='row'
+        alignItems='center'
+        justifyContent='center'
+      >
+        <NextImage src={GCentral_Logo} width={120} height={120} />
+        <Typography variant='h5' px={2}>
+          GCentral
+        </Typography>
+      </Box>
+    </Link>
+  )
+
+  const corgiBlock = (
+    <Link href={'https://corgibytes.com/'} py={2}>
+      <NextImage src={CorgiBytes_Logo} width={275} height={79} />
+    </Link>
+  )
+
+  const heartwareBlock = (
+    <Link href={'https://www.heartware.dev/'} py={2}>
+      <div className='bg-[#1e143e]'>
+        <NextImage src={HeartWare_Dev_Logo} width={1000} height={250} />
+      </div>
+    </Link>
+  )
+
+  const supportedBlock = (
+    <div className='flex flex-col text-center m-auto'>
+      <Typography variant='h5'>Supported by</Typography>
+      <Box
+        display='flex'
+        flexDirection='column' //</Link>{{ xs: 'column-reverse', md: 'row' }}
+        alignItems='center'
+        justifyContent='center'
+      >
+        {gcentralBlock}
+        {corgiBlock}
+        {heartwareBlock}
+      </Box>
+    </div>
+  )
+
   return (
     <>
-      <StackedBoxes>
-        <Countdown event_start={eventStart} event_end={eventEnd} />
-        <P>
-          The GLA Summit Organizers would like to welcome all LabVIEW
-          enthusiasts to join us in our third GLA Summit!
-          <br />
-          The Summit will run between 12:00 UTC, 14<sup>th</sup> and 12:00 UTC,
-          15<sup>th</sup> November 2022, for a full 24 hours.
-        </P>
-        <P>
-          This is an exciting opportunity for advanced LabVIEW developers and
-          Architects (certified or self-proclaimed) from around the world to
-          network and participate in an inclusive, all-digital, free event.
-        </P>
-        <P>
-          Presenters at the GLA Summit will be eligible to receive 30
-          recertification points for NI LabVIEW certifications.
-          <br />
-          Attendees will also be able to receive 20 points.
-        </P>
-        <Box>
-          <Link href={'https://hopin.com/events/gla-summit-2022'}>
-            <Button fullWidth variant='contained' className='bg-primaryc'>
-              Register for a ticket at Hopin
-            </Button>
-          </Link>
-        </Box>
-        {/* <Box>
-          <Link href={'/submit-presentation'}>
-            <Button fullWidth variant='contained'>
-              Submit a Presentation Now!
-            </Button>
-          </Link>
-        </Box> */}
-      </StackedBoxes>
-      {/* <SnackbarNotification open anchorOrigin={anchorPosition}>
-        <P textAlign='center'>
-          Our presentation submission process is closing today.
-        </P>
-        <P textAlign='center'>
-          If you are considering submitting a presentation, please do so now!
-        </P>
-      </SnackbarNotification> */}
-      <Box my='auto' pb={2}>
-        <Box>
-          <SponsorBar />
-        </Box>
-      </Box>
+      <Countdown event_start={eventStart} event_end={eventEnd} />
+      <div className='max-md:flex max-md:flex-col md:grid md:grid-cols-[1fr_2fr_1fr] md:grid-rows-1 w-full max-w-[90vw] mx-auto'>
+        <div className='contents-display md:grid md:col-start-2 my-auto'>
+          {mainBlock}
+        </div>
+        <div className='md:grid md:col-start-1 md:row-start-1 m-auto max-md:pb-4'>
+          {NI_Block}
+        </div>
+        <div className='md:grid md:col-start-3'>{supportedBlock}</div>
+      </div>
     </>
   )
 }
