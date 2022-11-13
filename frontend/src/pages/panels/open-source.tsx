5f61667b17fdfa680e588ee48152f40c9a3e3c22
Christian Butcher <chrisb2244@gmail.com>
2022-11-13 13:18:30 +0900

    Remove unused getStaticProps from the panel pages.

--- frontend/src/pages/panels/open-source.tsx
+++ frontend/src/pages/panels/open-source.tsx
@@ -1,64 +1,7 @@
-import type { Presentation, Presenter } from '@/Components/PresentationSummary'
-import type { GetStaticProps } from 'next'
 import { StackedBoxes } from '@/Components/Layout/StackedBoxes'
-import { getPublicPresentations } from '@/lib/databaseFunctions'
-import { YearGroupedPresentations } from '@/Components/Layout/YearGroupedPresentations'
-import { Box, Typography, Button } from '@mui/material'
-import { splitByYear } from '@/lib/presentationArrayFunctions'
-import NextLink from 'next/link'
+import { Typography, Button } from '@mui/material'
 
-type AllPresentationsProps = {
-  presentations: Presentation[]
-}
-
-export const getStaticProps: GetStaticProps<
-  AllPresentationsProps
-> = async () => {
-  const dbPresentations = await getPublicPresentations()
-  const presentations = dbPresentations.map((p) => {
-    const presenters = p.all_presenters_names.map((_, idx) => {
-      const presenter: Presenter = {
-        firstname: p.all_presenter_firstnames[idx],
-        lastname: p.all_presenter_lastnames[idx]
-      }
-      return presenter
-    })
-    const presentation: Presentation = {
-      title: p.title,
-      abstract: p.abstract,
-      speakers: presenters,
-      speakerNames: p.all_presenters_names,
-      presentationId: p.presentation_id,
-      year: p.year,
-      scheduledFor: p.scheduled_for,
-      presentationType: p.presentation_type
-    }
-    return presentation
-  })
-
-  const props = {
-    presentations
-  }
-  return { props, revalidate: 3600 }
-}
-
-const AllPresentations: React.FC<AllPresentationsProps> = ({
-  presentations
-}) => {
-  const presentationEntries = splitByYear(presentations)
-  const elems = presentationEntries.map(
-    ([year, presentationsInYear], idx, arr) => (
-      <Box pb={1} key={`presentationslist-${year}`}>
-        <YearGroupedPresentations
-          year={year}
-          presentations={presentationsInYear.slice(0, 3)}
-          initiallyOpen={idx === 0}
-          disableAccordion={arr.length === 1}
-        />
-      </Box>
-    )
-  )
-
+const Open_Source_Page = () => {
   /*
     Moderator: Mike Radziwon
     enrique.noe@pantherlab.com.mx 
@@ -148,4 +91,4 @@ const AllPresentations: React.FC<AllPresentationsProps> = ({
   )
 }
 
-export default AllPresentations
+export default Open_Source_Page
