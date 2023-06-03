c69a894f7ea83fd7595e7a3e06a64e79c7bdc638
Christian Butcher <chrisb2244@gmail.com>
2023-06-04 03:51:19 +0900

    Fix the display of the WaitingIndicator to be centered and on top of other elements (rather than inline/embedded with the caller).

--- frontend/src/Components/WaitingIndicator.tsx
+++ frontend/src/Components/WaitingIndicator.tsx
@@ -7,28 +7,26 @@ type WaitingIndicatorProps = {
   children?: React.ReactNode
 }
 
-export const WaitingIndicator: React.FC<
-  React.PropsWithChildren<WaitingIndicatorProps>
-> = ({
+export const WaitingIndicator: React.FC<WaitingIndicatorProps> = ({
   scale = 0.3,
-  maxLength = 300,
-  children
+  maxLength = 300
 }) => {
   const width = typeof screen === 'undefined' ? 0 : screen.width
   const length = Math.min(scale * width, maxLength)
 
   return (
-    <div>
-      <NextImage
-        src={GLA_Logo}
-        width={length}
-        height={length}
-        style={{
-          animation: 'spin 10s infinite linear'
-        }}
-        alt='loading indicator'
-      />
-      {children}
+    <div className='relative z-[100]'>
+      <div className='fixed inset-0 flex items-center justify-center p-4'>
+        <NextImage
+          src={GLA_Logo}
+          width={length}
+          height={length}
+          style={{
+            animation: 'spin 10s infinite linear'
+          }}
+          alt='loading indicator'
+        />
+      </div>
     </div>
   )
 }
