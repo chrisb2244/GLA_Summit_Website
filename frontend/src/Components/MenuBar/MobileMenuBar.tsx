f29927fd4b2fab5a86bfe1fd023c9675c95f04de
Christian Butcher <chrisb2244@gmail.com>
2023-06-25 10:27:00 +0900

    Rewrite the MobileMenuBar to remove dependencies on Mui and Mui-Icons in favour of an mdi icon and the headlessui Popover component.

--- frontend/src/Components/MenuBar/MobileMenuBar.tsx
+++ frontend/src/Components/MenuBar/MobileMenuBar.tsx
@@ -1,67 +1,48 @@
 'use client'
-// Needs useState for dropdown box
-
-import { useState } from 'react'
-import type { MouseEvent } from 'react'
-import { Typography, Box, IconButton, Menu, MenuItem } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
+// Needs useClient for the Popover component
+import { Popover } from '@headlessui/react'
+import { mdiMenu } from '@mdi/js'
+import { Icon } from '@mdi/react'
 import NextLink from 'next/link'
 
 export const MobileMenuBar = (props: {
   menuElements: Array<{ title: string; link: string }>
 }) => {
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null)
-
-  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget)
-  }
-  const handleCloseNavMenu = () => setAnchorElNav(null)
-
-  const boxProps = {
-    role: 'menu',
-    pl: 1,
-    alignContent: 'center',
-    flexGrow: 1
-  }
-
   return (
-    <Box id='mobile-menu' display={{ xs: 'flex', md: 'none' }} {...boxProps}>
-      <IconButton
-        aria-haspopup
-        aria-controls='menu-appbar'
-        aria-label='menu toggle button'
-        onClick={handleOpenNavMenu}
-        color='inherit'
-        size='large'
-      >
-        <MenuIcon />
-      </IconButton>
-      <Menu
-        id='menu-appbar'
-        anchorEl={anchorElNav}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-        open={Boolean(anchorElNav)}
-        onClose={handleCloseNavMenu}
-        keepMounted
-        sx={{ display: { xs: 'block', md: 'none' } }}
-      >
-        {props.menuElements.map(({ title, link }) => {
-          return (
-            <NextLink
-              href={link}
-              passHref
-              key={title}
-              legacyBehavior
-              prefetch={false}
-            >
-              <MenuItem onClick={() => handleCloseNavMenu()}>
-                <Typography textAlign='center'>{title}</Typography>
-              </MenuItem>
-            </NextLink>
-          )
-        })}
-      </Menu>
-    </Box>
+    <div
+      className='xs:flex md:hidden flex-grow pl-2 content-center'
+      role='menu'
+      id='mobile-menu'
+    >
+      <Popover>
+        <Popover.Button
+          aria-haspopup
+          aria-label='menu toggle button'
+        >
+          <Icon path={mdiMenu} size={1} />
+        </Popover.Button>
+        <Popover.Panel
+          className='absolute overflow-hidden rounded bg-white text-black text-opacity-75'
+          style={{
+            boxShadow:
+              '0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12)'
+          }}
+        >
+          {({ close }) => (
+            <div className='list-none cursor-pointer'>
+              {props.menuElements.map(({ title, link }) => {
+                return (
+                  <NextLink href={link} key={title} prefetch={false}>
+                    <li className='py-[6px] px-4' onClick={() => close()}>
+                      <p className='tracking-[0.00938em]'>{title}</p>
+                    </li>
+                  </NextLink>
+                )
+              })}
+            </div>
+          )}
+        </Popover.Panel>
+      </Popover>
+    </div>
   )
 }
