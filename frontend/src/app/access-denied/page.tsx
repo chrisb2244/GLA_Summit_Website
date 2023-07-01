92c40711113065cc938d2166da2437159db8faaf
Christian Butcher <chrisb2244@gmail.com>
2023-07-01 17:51:28 +0900

    Add spacing to the top of the access-denied page on larger screens.

--- frontend/src/app/access-denied/page.tsx
+++ frontend/src/app/access-denied/page.tsx
@@ -8,17 +8,20 @@ const AccessDeniedPage = () => {
   }
 
   return (
-    <StackedBoxes>
-      <div className='flex justify-center'>
-        <Icon path={mdiAlertCircle} size={2} />
-      </div>
-      <T>You do not have access to this page.</T>
-      <T>
-        If you believe you should have access, please check you are signed in,
-        and with the correct account.
-      </T>
-      <T>If you still cannot access this page, contact web@glasummit.org</T>
-    </StackedBoxes>
+    <div className='flex flex-col mb-auto'>
+      <div className='md:h-8' />
+      <StackedBoxes>
+        <div className='flex justify-center'>
+          <Icon path={mdiAlertCircle} size={2} />
+        </div>
+        <T>You do not have access to this page.</T>
+        <T>
+          If you believe you should have access, please check you are signed in,
+          and with the correct account.
+        </T>
+        <T>If you still cannot access this page, contact web@glasummit.org</T>
+      </StackedBoxes>
+    </div>
   )
 }
 
