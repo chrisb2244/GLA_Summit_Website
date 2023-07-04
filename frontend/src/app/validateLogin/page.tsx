0093ee31ebe6af44e2acb17cb4c52a6dec5147b3
Christian Butcher <chrisb2244@gmail.com>
2023-07-05 01:47:05 +0900

    Add a basic validation page for the login code.

---
+++ frontend/src/app/validateLogin/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { verifyLogin2 } from '@/Components/SigninRegistration/SignInUpActions'
+import { useForm } from 'react-hook-form'
+
+type ValidationFormData = {
+  email: string
+  verificationCode: string
+}
+
+const ValidateLogin = () => {
+  const onSubmit = (data: ValidationFormData) => {
+    console.log(data)
+    verifyLogin2(data)
+  }
+
+  const { register, handleSubmit } = useForm<ValidationFormData>()
+
+  return (
+  <form onSubmit={handleSubmit(onSubmit)}>
+    <input {...register('email')} />
+    <input {...register('verificationCode')} className='border border-black' />
+    <button type='submit'>Submit</button>
+  </form>
+  )
+}
+
+export default ValidateLogin
