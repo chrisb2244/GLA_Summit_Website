195f7e5585cf67c918d000f3658d1c64e24253f3
Christian Butcher <chrisb2244@gmail.com>
2023-07-05 03:01:06 +0900

    Partial migration of the NewUserRegistration Form.

--- frontend/src/Components/SigninRegistration/NewUserRegistration.tsx
+++ frontend/src/Components/SigninRegistration/NewUserRegistration.tsx
@@ -1,4 +1,5 @@
-import { Dialog, Button, Typography, Container, Link, Box } from '@mui/material'
+import { Button } from '@mui/material'
+import { Dialog } from '@headlessui/react'
 import { TypedFieldPath, useForm } from 'react-hook-form'
 import type { SubmitHandler, SubmitErrorHandler } from 'react-hook-form'
 import type { ApiError, UserCredentials } from '@/lib/sessionContext'
@@ -8,15 +9,15 @@ import { myLog } from '@/lib/utils'
 import { useCallback, useState } from 'react'
 import { NotificationDialogPopup } from '../NotificationDialogPopup'
 import type { SignUpOptions, SignUpReturn } from '@/lib/sessionContext'
-import { WaitingIndicator } from '../WaitingIndicator'
 import NextLink from 'next/link'
 import { SmallCenteredText } from '@/Components/Utilities/SmallCenteredText'
+import { LinkLikeText } from '../Utilities/LinkLikeText'
+import { signUp } from './SignInUpActions'
 
 type UserRegistrationProps = {
   open: boolean
   setClosed: () => void
   switchToSignIn: () => void
-  signUp: SignUpFunction
   waitingSpinner: JSX.Element
 }
 
@@ -34,19 +35,16 @@ function EMPTY<T>() {
   return '' as TypedFieldPath<T, T>
 }
 
-export const NewUserRegistration: React.FC<React.PropsWithChildren<UserRegistrationProps>> = ({
-  signUp,
-  setClosed,
-  waitingSpinner,
-  ...props
-}) => {
+export const NewUserRegistration: React.FC<
+  React.PropsWithChildren<UserRegistrationProps>
+> = ({ open, setClosed, waitingSpinner, ...props }) => {
   const {
     handleSubmit,
     reset,
     register,
     formState: { errors }
   } = useForm<PersonProps>({
-    mode: 'onTouched',
+    mode: 'onTouched'
   })
   const [isWaiting, setIsWaiting] = useState(false)
   const [popupEmail, setPopupOpen] = useState<string | null>(null)
@@ -59,22 +57,17 @@ export const NewUserRegistration: React.FC<React.PropsWithChildren<UserRegistrat
       }
 
       const redirectTo = new URL(window.location.href).origin + '/'
-      signUp(
-        { email: data.email, password: randomBytes(32).toString('hex') },
-        { data: newUserData, redirectTo }
-      )
+      signUp(data.email, newUserData, redirectTo)
         // .then(({ user, session, error }) => {
-        .then(({ error }) => {
+        .then((success) => {
           // myLog({ user, session, error })
-          if (error) throw error
-          // If reaching here, no error
-          // console.log('Check your email for the login link!')
-          setIsWaiting(false)
-          setPopupOpen(data.email)
-        })
-        .catch((error: ApiError) => {
-          myLog(error.message)
-          setIsWaiting(false)
+          if (!success) {
+            console.log("Failed to sign up")
+            setIsWaiting(false)
+          } else {
+            setIsWaiting(false)
+            setPopupOpen(data.email)
+          }
         })
       setClosed()
       reset()
@@ -88,82 +81,93 @@ export const NewUserRegistration: React.FC<React.PropsWithChildren<UserRegistrat
   }
 
   const onError: SubmitErrorHandler<PersonProps> = (err) => myLog(err)
-  const BoldEmail = (props: { email: string | null }) => {
-    return (
-      <Box component='span' fontWeight={500}>
-        {props.email}
-      </Box>
-    )
-  }
 
   return (
     <>
-      <Dialog open={props.open} onClose={() => {
-        setClosed()
-        reset({firstName: '', lastName: '', email: ''})
-      }}>
-        <Container maxWidth='md' sx={{ p: 2 }}>
-          <SmallCenteredText sx={{ pb: 2 }}>
-            Already registered?{' '}
-            <Link
-              onClick={(ev) => {
-                ev.preventDefault()
-                props.switchToSignIn()
-              }}
-              component='button'
-            >
-              Sign In
-            </Link>
-          </SmallCenteredText>
-          <SmallCenteredText sx={{ pb: 1 }}>
-            Please fill out the information below. You will receive an email
-            with a verification link - click the link to automatically sign into
-            the site.
-          </SmallCenteredText>
-          <div className='bg-red-600 text-white rounded my-2 pt-2'>
-          <SmallCenteredText sx={{ pb: 1 }}>
-            In order to attend the conference, the required registration can be
-            found at{' '}
-            {
-              <a href={'https://hopin.com/events/gla-summit-2022'} className='underline'>
-                https://hopin.com/events/gla-summit-2022
-              </a>
-            }
-          </SmallCenteredText>
-          </div>
-          <SmallCenteredText sx={{ pb: 2 }}>
-            This site is currently mostly focused on presentation submission,
-            although last year&apos;s presentations can be found at{' '}
-            {
-              <NextLink href={'/presentations'}>
-                <span className='underline cursor-pointer'>https://glasummit.org/presentations</span>
-              </NextLink>
-            }
-            .
-          </SmallCenteredText>
-          <form onSubmit={handleSubmit(onSubmit, onError)}>
-            <Person<PersonProps>
-              register={register}
-              path={EMPTY()}
-              errors={errors}
-              splitSize={null}
-              sx={{ pb: 2 }}
-            />
-            <Button type='submit' variant='outlined' fullWidth>
-              Register
-            </Button>
-          </form>
-        </Container>
+      <Dialog
+        as='div'
+        className='relative z-[100]'
+        open={open}
+        onClose={() => {
+          setClosed()
+          reset({ firstName: '', lastName: '', email: '' })
+        }}
+      >
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        <div className='fixed inset-0 bg-black/30' aria-hidden='true' />
+
+        {/* Full-screen container to center the panel */}
+        <div className='fixed inset-0 flex items-center justify-center p-4'>
+          <Dialog.Panel className='bg-white p-4 rounded max-w-xl' id='registerDialog'>
+            <SmallCenteredText sx={{ pb: 2 }}>
+              Already registered?{' '}
+              <LinkLikeText
+                onClick={(ev) => {
+                  ev.preventDefault()
+                  props.switchToSignIn()
+                }}
+              >
+                Sign In
+              </LinkLikeText>
+            </SmallCenteredText>
+            <SmallCenteredText sx={{ pb: 1 }}>
+              Please fill out the information below. You will receive an email
+              with a verification link - click the link to automatically sign
+              into the site.
+            </SmallCenteredText>
+            <div className='bg-red-600 text-white rounded my-2 pt-2'>
+              <SmallCenteredText sx={{ pb: 1 }}>
+                In order to attend the conference, the required registration can
+                be found at{' '}
+                {
+                  <a
+                    href={'https://hopin.com/events/gla-summit-2022'}
+                    className='underline'
+                  >
+                    https://hopin.com/events/gla-summit-2022
+                  </a>
+                }
+              </SmallCenteredText>
+            </div>
+            <SmallCenteredText sx={{ pb: 2 }}>
+              This site is currently mostly focused on presentation submission,
+              although last year&apos;s presentations can be found at{' '}
+              {
+                <NextLink href={'/presentations'}>
+                  <span className='underline cursor-pointer'>
+                    https://glasummit.org/presentations
+                  </span>
+                </NextLink>
+              }
+              .
+            </SmallCenteredText>
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
+              <Person<PersonProps>
+                register={register}
+                path={EMPTY()}
+                errors={errors}
+                splitSize={null}
+                sx={{ pb: 2 }}
+              />
+              {/* TODO: Text colour used here as a blue, also border colour */}
+              <button type='submit' className='w-full border rounded border-blue-300 uppercase px-4 py-1 text-blue-500'>
+                Register
+              </button>
+              {/* <Button type='submit' variant='outlined' fullWidth>
+                Register
+              </Button> */}
+            </form>
+          </Dialog.Panel>
+        </div>
       </Dialog>
       <NotificationDialogPopup
         open={popupEmail !== null}
         onClose={() => setPopupOpen(null)}
       >
-        <Typography>
+        <p>
           Thank you for registering for the GLA Summit 2022 website. Please
-          check your email at <BoldEmail email={popupEmail} /> to verify your
-          account.
-        </Typography>
+          check your email at <b>{popupEmail}</b> to verify your account.
+        </p>
       </NotificationDialogPopup>
       {isWaiting ? waitingSpinner : null}
     </>
