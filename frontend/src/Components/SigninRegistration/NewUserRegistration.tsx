2cd6afe455491fc239348d3f0f07010bd3320822
Christian Butcher <chrisb2244@gmail.com>
2023-07-10 01:13:27 +0900

    Add a new 'CenteredDialog' component for use with popups. Refactor the NewUserRegistration to use this.

--- frontend/src/Components/SigninRegistration/NewUserRegistration.tsx
+++ frontend/src/Components/SigninRegistration/NewUserRegistration.tsx
@@ -1,5 +1,3 @@
-import { Button } from '@mui/material'
-import { Dialog } from '@headlessui/react'
 import { TypedFieldPath, useForm } from 'react-hook-form'
 import type { SubmitHandler, SubmitErrorHandler } from 'react-hook-form'
 import type { ApiError, UserCredentials } from '@/lib/sessionContext'
@@ -13,6 +11,7 @@ import NextLink from 'next/link'
 import { SmallCenteredText } from '@/Components/Utilities/SmallCenteredText'
 import { LinkLikeText } from '../Utilities/LinkLikeText'
 import { signUp } from './SignInUpActions'
+import { CenteredDialog } from '../Layout/CenteredDialog'
 
 type UserRegistrationProps = {
   open: boolean
@@ -84,82 +83,77 @@ export const NewUserRegistration: React.FC<
 
   return (
     <>
-      <Dialog
-        as='div'
-        className='relative z-[100]'
+      <CenteredDialog
         open={open}
         onClose={() => {
           setClosed()
           reset({ firstName: '', lastName: '', email: '' })
         }}
+        dialogId='registerDialog'
       >
-        {/* The backdrop, rendered as a fixed sibling to the panel container */}
-        <div className='fixed inset-0 bg-black/30' aria-hidden='true' />
-
-        {/* Full-screen container to center the panel */}
-        <div className='fixed inset-0 flex items-center justify-center p-4'>
-          <Dialog.Panel className='bg-white p-4 rounded max-w-xl' id='registerDialog'>
-            <SmallCenteredText sx={{ pb: 2 }}>
-              Already registered?{' '}
-              <LinkLikeText
-                onClick={(ev) => {
-                  ev.preventDefault()
-                  props.switchToSignIn()
-                }}
+        <SmallCenteredText sx={{ pb: 2 }}>
+          Already registered?{' '}
+          <LinkLikeText
+            onClick={(ev) => {
+              ev.preventDefault()
+              props.switchToSignIn()
+            }}
+          >
+            Sign In
+          </LinkLikeText>
+        </SmallCenteredText>
+        <SmallCenteredText sx={{ pb: 1 }}>
+          Please fill out the information below. You will receive an email with
+          a verification link - click the link to automatically sign into the
+          site.
+        </SmallCenteredText>
+        <div className='bg-red-600 text-white rounded my-2 pt-2'>
+          <SmallCenteredText sx={{ pb: 1 }}>
+            In order to attend the conference, the required registration can be
+            found at{' '}
+            {
+              <a
+                href={'https://hopin.com/events/gla-summit-2022'}
+                className='underline'
               >
-                Sign In
-              </LinkLikeText>
-            </SmallCenteredText>
-            <SmallCenteredText sx={{ pb: 1 }}>
-              Please fill out the information below. You will receive an email
-              with a verification link - click the link to automatically sign
-              into the site.
-            </SmallCenteredText>
-            <div className='bg-red-600 text-white rounded my-2 pt-2'>
-              <SmallCenteredText sx={{ pb: 1 }}>
-                In order to attend the conference, the required registration can
-                be found at{' '}
-                {
-                  <a
-                    href={'https://hopin.com/events/gla-summit-2022'}
-                    className='underline'
-                  >
-                    https://hopin.com/events/gla-summit-2022
-                  </a>
-                }
-              </SmallCenteredText>
-            </div>
-            <SmallCenteredText sx={{ pb: 2 }}>
-              This site is currently mostly focused on presentation submission,
-              although last year&apos;s presentations can be found at{' '}
-              {
-                <NextLink href={'/presentations'}>
-                  <span className='underline cursor-pointer'>
-                    https://glasummit.org/presentations
-                  </span>
-                </NextLink>
-              }
-              .
-            </SmallCenteredText>
-            <form onSubmit={handleSubmit(onSubmit, onError)}>
-              <Person<PersonProps>
-                register={register}
-                path={EMPTY()}
-                errors={errors}
-                splitSize={null}
-                sx={{ pb: 2 }}
-              />
-              {/* TODO: Text colour used here as a blue, also border colour */}
-              <button type='submit' className='w-full border rounded border-blue-300 uppercase px-4 py-1 text-blue-500'>
-                Register
-              </button>
-              {/* <Button type='submit' variant='outlined' fullWidth>
+                https://hopin.com/events/gla-summit-2022
+              </a>
+            }
+          </SmallCenteredText>
+        </div>
+        <SmallCenteredText sx={{ pb: 2 }}>
+          This site is currently mostly focused on presentation submission,
+          although last year&apos;s presentations can be found at{' '}
+          {
+            <NextLink href={'/presentations'}>
+              <span className='underline cursor-pointer'>
+                https://glasummit.org/presentations
+              </span>
+            </NextLink>
+          }
+          .
+        </SmallCenteredText>
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
+          <Person<PersonProps>
+            register={register}
+            path={EMPTY()}
+            errors={errors}
+            splitSize={null}
+            sx={{ pb: 2 }}
+          />
+          {/* TODO: Text colour used here as a blue, also border colour */}
+          <button
+            type='submit'
+            className='w-full border rounded border-blue-300 uppercase px-4 py-1 text-blue-500'
+          >
+            Register
+          </button>
+          {/* <Button type='submit' variant='outlined' fullWidth>
                 Register
               </Button> */}
-            </form>
-          </Dialog.Panel>
-        </div>
-      </Dialog>
+        </form>
+      </CenteredDialog>
+
       <NotificationDialogPopup
         open={popupEmail !== null}
         onClose={() => setPopupOpen(null)}
