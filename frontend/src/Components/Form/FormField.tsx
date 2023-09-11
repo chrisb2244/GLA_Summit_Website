8148b073f379c2e83be2160ea5279794838797c7
Christian Butcher <chrisb2244@gmail.com>
2023-09-11 11:38:00 +0900

    Rework the FormField input components to handle a passed placeholder. Adjust the top border line by removing unnecessary classes from the dummy element.

--- frontend/src/Components/Form/FormField.tsx
+++ frontend/src/Components/Form/FormField.tsx
@@ -3,20 +3,66 @@ import { type VariantProps, cva } from 'class-variance-authority'
 import type { HTMLInputTypeAttribute, HTMLProps } from 'react'
 import React from 'react'
 
-const inputFieldStyles = cva(
-  'px-4 pt-3 pb-1 peer placeholder-transparent border focus-visible:outline-none border-t-0 focus:border-t-0 placeholder-shown:border-t text-lg border-gray-400 box-border',
-  {
-    variants: {
-      fullWidth: {
-        true: 'w-full'
-      },
-      readOnly: {
-        true: 'text-gray-500',
-        false: 'border-b-4 focus:border-b-secondaryc'
-      }
+// Define separately for use styling the label and the error message
+const errorTextClassNames = 'text-red-700'
+
+const inputAlways = [
+  'px-4 pt-3 pb-1 peer border focus-visible:outline-none',
+  'border-t-0 focus:border-t-0 placeholder-shown:border-t-0',
+  'text-lg border-gray-400 box-border'
+].join(' ')
+
+const inputFieldStyles = cva(inputAlways, {
+  variants: {
+    fullWidth: {
+      true: 'w-full'
+    },
+    readOnly: {
+      true: 'text-gray-500',
+      false: 'border-b-4 focus:border-b-secondaryc'
+    },
+    placeholderVisible: {
+      false: 'placeholder-transparent',
+      undefined: 'placeholder-transparent',
+      true: ''
     }
   }
-)
+})
+
+// IMPORTANT:
+// The peer selectors require that the input is before the label in the DOM.
+const placeholderShownClassNames = [
+  'peer-placeholder-shown:text-base',
+  'peer-placeholder-shown:top-4',
+  'peer-placeholder-shown:left-4',
+  'before:peer-placeholder-shown:hidden'
+].join(' ')
+
+const labelAlways = [
+  'absolute block z-[2] transition-all text-gray-700',
+  'text-sm -top-2 left-2 px-1 peer-focus:text-gray-700',
+  'peer-focus:text-sm peer-focus:-top-2 peer-focus:left-2',
+  'before:w-[105%] before:absolute before:flex before:top-2',
+  'before:-left-[2px] before:z-[-1] before:bg-white',
+  'before:h-[1px] before:peer-focus:w-[105%]',
+  'before:peer-focus:absolute before:peer-focus:flex',
+  'before:peer-focus:top-2 before:peer-focus:-left-[2px]',
+  'before:peer-focus:z-[-1] before:peer-focus:bg-white',
+  'before:peer-focus:h-[1px] peer-placeholder-shown:text-gray-500'
+].join(' ')
+
+const labelStyles = cva(labelAlways, {
+  variants: {
+    isError: {
+      true: errorTextClassNames
+    },
+    placeholderVisible: {
+      true: '',
+      false: placeholderShownClassNames,
+      undefined: placeholderShownClassNames
+    }
+  }
+})
 
 const wrapperStyles = cva(
   'inline-flex flex-col relative align-top mt-2 mb-4 bg-inherit overflow-x-clip',
@@ -51,6 +97,7 @@ export const FormField: React.FC<FormFieldProps> = (props) => {
   const { registerReturn, fieldError, fullWidth, ...inputProps } = props
   const isError = typeof fieldError !== 'undefined'
   const id = registerReturn.name
+  const placeholderVisible = typeof props.placeholder !== 'undefined'
 
   // placeholder-shown implies the field is empty
   // focus should have the non-empty classNames
@@ -64,7 +111,8 @@ export const FormField: React.FC<FormFieldProps> = (props) => {
         type={props.type ?? 'text'}
         className={inputFieldStyles({
           fullWidth,
-          readOnly: inputProps.readOnly ?? false
+          readOnly: inputProps.readOnly ?? false,
+          placeholderVisible
         })}
         placeholder={props.placeholder ?? id}
         {...registerReturn}
@@ -76,9 +124,7 @@ export const FormField: React.FC<FormFieldProps> = (props) => {
       <label
         id={`${id}-label`}
         htmlFor={id}
-        className={`${
-          isError ? errorTextClassNames : ''
-        } ${labelAlways} ${labelRaised} ${labelPlaceholder}`}
+        className={labelStyles({ isError, placeholderVisible })}
       >
         {props.label ?? id}
       </label>
@@ -107,11 +153,7 @@ export const FormFieldIndicator: React.FC<FormFieldIndicatorProps> = (
         {...inputProps}
       />
       {topBorderElement}
-      <label
-        id={`${id}-label`}
-        htmlFor={id}
-        className={`${labelAlways} ${labelRaised} ${labelPlaceholder}`}
-      >
+      <label id={`${id}-label`} htmlFor={id} className={labelStyles()}>
         {props.label ?? id}
       </label>
     </div>
@@ -126,6 +168,7 @@ export const TextArea: React.FC<TextAreaProps> = (props) => {
   const { registerReturn, fieldError, fullWidth, ...inputProps } = props
   const isError = typeof fieldError !== 'undefined'
   const id = registerReturn.name
+  const placeholderVisible = typeof props.placeholder !== 'undefined'
 
   return (
     <div className={wrapperStyles({ fullWidth, hidden: props.hidden })}>
@@ -133,7 +176,8 @@ export const TextArea: React.FC<TextAreaProps> = (props) => {
         id={id}
         className={inputFieldStyles({
           fullWidth,
-          readOnly: inputProps.readOnly ?? false
+          readOnly: inputProps.readOnly ?? false,
+          placeholderVisible
         })}
         placeholder={props.placeholder ?? id}
         {...registerReturn}
@@ -143,9 +187,7 @@ export const TextArea: React.FC<TextAreaProps> = (props) => {
       <label
         id={`${id}-label`}
         htmlFor={id}
-        className={`${
-          isError ? errorTextClassNames : ''
-        } ${labelAlways} ${labelRaised} ${labelPlaceholder}`}
+        className={labelStyles({ isError, placeholderVisible })}
       >
         {props.label ?? id}
       </label>
@@ -163,37 +205,10 @@ const ErrorMessage = ({ error }: { error: FieldError | undefined }) => {
   ) : null
 }
 
-// IMPORTANT:
-// The peer selectors require that the input is before the label in the DOM.
-const labelAlways = 'absolute block z-[2] transition-all'
-const labelRaised = [
-  'text-gray-700',
-  'text-sm',
-  '-top-2',
-  'left-2',
-  'px-1',
-  'peer-focus:text-gray-700',
-  'peer-focus:text-sm',
-  'peer-focus:-top-2',
-  'peer-focus:left-2',
-  'before:w-[105%] before:absolute before:flex before:top-2 before:-left-[2px] before:z-[-1] before:bg-white before:h-[1px]',
-  'before:peer-focus:w-[105%] before:peer-focus:absolute before:peer-focus:flex before:peer-focus:top-2 before:peer-focus:-left-[2px] before:peer-focus:z-[-1] before:peer-focus:bg-white before:peer-focus:h-[1px]'
-].join(' ')
-const labelPlaceholder = [
-  'peer-placeholder-shown:text-gray-500',
-  'peer-placeholder-shown:text-base',
-  'peer-placeholder-shown:top-4',
-  'peer-placeholder-shown:left-4',
-  'before:peer-placeholder-shown:hidden'
-].join(' ')
-
-// Define separately for use styling the label and the error message
-const errorTextClassNames = 'text-red-700'
-
 // Element to provide a top border when the label moves upwards
 const topBorderElement = (
   <div
-    className='absolute top-0 left-0 bg-gray-400 h-[1px] w-full peer-focus:absolute peer-focus:top-0 peer-focus:left-0 peer-focus:bg-gray-400 peer-focus:h-[1px] peer-focus:w-full peer-focus:block peer-placeholder-shown:bg-transparent'
+    className='absolute left-0 top-0 h-[1px] w-full bg-gray-400 peer-focus:block'
     aria-hidden
   />
 )
