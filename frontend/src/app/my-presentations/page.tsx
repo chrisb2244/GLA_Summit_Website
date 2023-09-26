81b485b3657fcc804da6b2ddd93f5062de30a43a
Christian Butcher <chrisb2244@gmail.com>
2023-09-27 01:00:30 +0900

    Add a brief message indicating that presentation submission is coming soon (and not currently available).

--- frontend/src/app/my-presentations/page.tsx
+++ frontend/src/app/my-presentations/page.tsx
@@ -1,5 +1,10 @@
 const MyPresentationsPage = () => {
-  return null
+  return (
+    <div className='prose mx-auto'>
+      <p>The presentation submission page is currently being reworked!</p>
+      <p>We look forwards to being able to accept submissions soon.</p>
+    </div>
+  )
 }
 
 export default MyPresentationsPage
