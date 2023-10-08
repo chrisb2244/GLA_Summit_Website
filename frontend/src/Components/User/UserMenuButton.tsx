90b4ee7ceda9a0e8e17c0124103674567d3201de
Christian Butcher <chrisb2244@gmail.com>
2023-10-08 14:12:37 +0900

    Switch the user layout component back to server-rendered + RSC.

--- frontend/src/Components/User/UserMenuButton.tsx
+++ frontend/src/Components/User/UserMenuButton.tsx
@@ -1,65 +1,31 @@
-'use client'
-
 import { SignInUpButton } from '@/Components/SigninRegistration/SignInUpButton'
-import {
-  User,
-  createClientComponentClient
-} from '@supabase/auth-helpers-nextjs'
 import { WaitingIndicator } from '@/Components/Utilities/WaitingIndicator'
 import { UserMenu } from '@/Components/User/UserMenu'
-import { useEffect, useState } from 'react'
 import {
   getIsOrganizer,
-  getUser,
   signOut
 } from '@/Components/SigninRegistration/SignInUpActions'
-import { useRouter } from 'next/navigation'
+import { createServerComponentClient } from '@/lib/supabaseServer'
 
-export function UserMenuButton() {
-  const [isLoading, setIsLoading] = useState(true)
-
-  const [user, setUser] = useState<User | null>()
-  const [isOrganizer, setIsOrganizer] = useState(false)
-
-  const supabase = createClientComponentClient()
-  const router = useRouter()
-
-  const signOutClient = async () => {
-    await signOut().then(() => {
-      setUser(null)
-      router.refresh()
-    })
-  }
+export async function UserMenuButton() {
+  const supabase = createServerComponentClient()
 
   const updateUserClient = async () => {
-    getUser()
-      .then(setUser)
-      .then(() => {
-        setIsLoading(false)
-        router.refresh()
-      })
+    "use server"
+    console.log("Updating user login...")
   }
 
-  useEffect(() => {
-    updateUserClient()
-  }, [supabase, router])
-
-  useEffect(() => {
-    if (typeof user === 'undefined' || user === null) {
-      setIsOrganizer(false)
-      return
-    }
-    getIsOrganizer(user).then(setIsOrganizer)
-  }, [user])
+  const user = (await supabase.auth.getUser()).data.user
+  const isOrganizer = user === null ? false : await getIsOrganizer(user);
 
   const idleSpinner = <WaitingIndicator />
-  const button = isLoading ? null : user == null ? (
+  const button = user == null ? (
     <SignInUpButton
       waitingSpinner={idleSpinner}
       onSignInComplete={updateUserClient}
     />
   ) : (
-    <UserMenu user={user} isOrganizer={isOrganizer} signOut={signOutClient} />
+    <UserMenu user={user} isOrganizer={isOrganizer} signOut={signOut} />
   )
 
   return (
