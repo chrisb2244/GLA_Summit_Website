e74522157d190fe8b553c902855546d1e6267b7d
Christian Butcher <chrisb2244@gmail.com>
2023-10-11 03:10:20 +0900

    Avoid calling a ServerAction function not as a ServerAction. Replace that call with a direct use of the existing ServerComponent client, which avoids triggering the cookie error.

--- frontend/src/Components/User/UserMenuButton.tsx
+++ frontend/src/Components/User/UserMenuButton.tsx
@@ -1,11 +1,9 @@
 import { SignInUpButton } from '@/Components/SigninRegistration/SignInUpButton';
 import { WaitingIndicator } from '@/Components/Utilities/WaitingIndicator';
 import { UserMenu } from '@/Components/User/UserMenu';
-import {
-  getIsOrganizer,
-  signOut
-} from '@/Components/SigninRegistration/SignInUpActions';
+import { signOut } from '@/Components/SigninRegistration/SignInUpActions';
 import { createServerComponentClient } from '@/lib/supabaseServer';
+import { User } from '@supabase/supabase-js';
 
 export async function UserMenuButton() {
   const supabase = createServerComponentClient();
@@ -16,7 +14,22 @@ export async function UserMenuButton() {
   };
 
   const user = (await supabase.auth.getUser()).data.user;
-  const isOrganizer = user === null ? false : await getIsOrganizer(user);
+
+  const getIsOrganizer = async (user: User | null) => {
+    if (user === null) {
+      return false;
+    }
+    const { data, error } = await supabase
+      .from('organizers')
+      .select()
+      .eq('id', user.id);
+    if (error) {
+      console.log({ getOrganizerError: error });
+      return false;
+    }
+    return data.length === 1;
+  };
+  const isOrganizer = await getIsOrganizer(user);
 
   const idleSpinner = <WaitingIndicator />;
   const button =
