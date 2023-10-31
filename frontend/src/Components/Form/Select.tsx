8d5ef7c1f41d0c9ead952be65677a9f750bb2687
Christian Butcher <chrisb2244@gmail.com>
2023-10-31 20:53:03 +0900

    Add a 'Select' component.

---
+++ frontend/src/Components/Form/Select.tsx
@@ -0,0 +1,39 @@
+import { VariantProps, cva } from 'class-variance-authority';
+import { HTMLProps } from 'react';
+
+type Option = {
+  key: string;
+  description: string;
+};
+
+type SelectProps = {
+  options: Option[];
+};
+
+type ExtendedSelectProps = SelectProps &
+  HTMLProps<HTMLSelectElement> &
+  VariantProps<typeof selectStyles>;
+
+export const Select = (props: ExtendedSelectProps) => {
+  const { options, fullWidth, ...inputProps } = props;
+  const optionElements = options.map(({ key, description }) => {
+    return (
+      <option key={key} value={key}>
+        {description}
+      </option>
+    );
+  });
+  return (
+    <select {...inputProps} className={selectStyles({ fullWidth })}>
+      {optionElements}
+    </select>
+  );
+};
+
+const selectStyles = cva('border border-gray-400 box-border p-2', {
+  variants: {
+    fullWidth: {
+      true: 'w-full'
+    }
+  }
+});
