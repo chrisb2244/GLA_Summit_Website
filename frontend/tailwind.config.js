3282c8e004b39421517cf355488d1cfe5759f500
Christian Butcher <chrisb2244@gmail.com>
2023-11-19 19:24:53 +0900

    Soften the default shadow values

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extend: {
       boxShadow: {
         DEFAULT:
-          '0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12)'
+          '0px 5px 5px -3px rgba(0,0,0,0.1),0px 8px 10px 1px rgba(0,0,0,0.07),0px 3px 14px 2px rgba(0,0,0,0.06)'
       },
       colors: {
         primaryc: '#5837b9',
