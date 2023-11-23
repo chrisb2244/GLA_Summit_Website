abf5023be43fad3438c6e532c164fae1f5af53d9
Christian Butcher <chrisb2244@gmail.com>
2023-11-23 23:21:26 +0900

    Readd the top-level menu link to /presenters.

--- frontend/src/Components/MenuBar/MenuBar.tsx
+++ frontend/src/Components/MenuBar/MenuBar.tsx
@@ -11,12 +11,12 @@ export const MenuBar = () => {
   const menuElements: MenuElement[] = [
     { title: 'Home', link: '/' },
     // { title: 'Agenda', link: '/full-agenda' },
-    { title: 'Presentations', link: '/presentations' }
+    { title: 'Presentations', link: '/presentations' },
     // { title: 'Submit a Presentation', link: '/submit-presentation' },
     // { title: 'Virtual Venue (Hopin)', link: 'https://hopin.com/events/gla-summit-2022' },
     // { title: 'Media and Banners', link: '/media' },
     // { title: 'Our Team', link: '/our-team' }
-    // { title: 'Presenters', link: '/presenters' }
+    { title: 'Presenters', link: '/presenters' }
   ];
 
   // Split these components since the "Mobile" version requires interactivity,
