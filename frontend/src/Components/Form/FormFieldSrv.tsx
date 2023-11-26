cb70203ed19c1b03ff8ac81b87cc054d05d27b01
Christian Butcher <chrisb2244@gmail.com>
2023-11-26 17:34:45 +0900

    Add server-friendly Person and FormField components.

---
+++ frontend/src/Components/Form/FormFieldSrv.tsx
@@ -0,0 +1,194 @@
+import { type VariantProps, cva } from 'class-variance-authority';
+import type { HTMLInputTypeAttribute, HTMLProps } from 'react';
+import React from 'react';
+
+// Define separately for use styling the label and the error message
+const errorTextColor = 'text-red-700';
+const errorTextClassName =
+  'text-red-700 absolute top-0 left-2 text-sm sm:text-base';
+
+const inputAlways = [
+  'px-4 pt-3 pb-1 peer border focus-visible:outline-none',
+  'border-t-0 focus:border-t-0 placeholder-shown:border-t-0',
+  'text-lg border-gray-400 box-border'
+].join(' ');
+
+const inputFieldStyles = cva(inputAlways, {
+  variants: {
+    fullWidth: {
+      true: 'w-full'
+    },
+    readOnly: {
+      true: 'text-gray-500 bg-gray-100',
+      false: 'border-b-4 focus:border-b-secondaryc'
+    },
+    placeholderVisible: {
+      false: 'placeholder-transparent',
+      undefined: 'placeholder-transparent',
+      true: ''
+    }
+  }
+});
+
+// IMPORTANT:
+// The peer selectors require that the input is before the label in the DOM.
+const placeholderShownClassNames = [
+  'peer-placeholder-shown:text-base',
+  'peer-placeholder-shown:top-4',
+  'peer-placeholder-shown:left-4',
+  'before:peer-placeholder-shown:hidden'
+].join(' ');
+
+const labelAlways = [
+  'absolute block z-[2] transition-all text-gray-700',
+  'text-sm -top-2 left-2 px-1 peer-focus:text-gray-700',
+  'peer-focus:text-sm peer-focus:-top-2 peer-focus:left-2',
+  'before:w-[105%] before:absolute before:flex before:top-2',
+  'before:-left-[2px] before:z-[-1]',
+  'before:h-[10px] before:rounded-b before:peer-focus:w-[105%]',
+  'before:peer-focus:absolute before:peer-focus:flex',
+  'before:peer-focus:top-2 before:peer-focus:-left-[2px]',
+  'before:peer-focus:z-[-1] before:peer-focus:bg-white',
+  'before:peer-focus:h-[10px] before:peer-focus:rounded-b',
+  'peer-placeholder-shown:text-gray-500'
+].join(' ');
+
+const labelStyles = cva(labelAlways, {
+  variants: {
+    isError: {
+      true: errorTextColor
+    },
+    placeholderVisible: {
+      true: '',
+      false: placeholderShownClassNames,
+      undefined: placeholderShownClassNames
+    },
+    readOnly: {
+      true: 'before:bg-gray-100',
+      false: 'before:bg-white',
+      undefined: 'before:bg-white'
+    }
+  }
+});
+
+const wrapperStyles = cva(
+  'inline-flex flex-col relative align-top mt-2 mb-5 bg-inherit overflow-x-clip',
+  {
+    variants: {
+      fullWidth: {
+        true: 'w-full'
+      },
+      hidden: {
+        true: 'hidden'
+      }
+    }
+  }
+);
+
+const extractSidePadding = (cname?: string) => {
+  return cname
+    ? cname.match(/(((xs|sm|md|lg|xl):)?p[lr]?\-[0-9]+)/g)?.reduce((s, ns) => {
+        return `${s} ${ns}`;
+      }, '')
+    : '';
+};
+const calculateBorderMargin = (cname?: string) => {
+  return extractSidePadding(cname)?.replaceAll(/p/g, 'm');
+};
+const calculateLabelPadding = (cname?: string) => {
+  const rawPadding = extractSidePadding(cname);
+
+  if (typeof rawPadding === 'undefined') {
+    return '';
+  }
+  return rawPadding.split(' ').reduce((s, ns) => {
+    const detectedLeftPaddingScale = ns.match(/((xs|sm|md|lg|xl):)pl-([0-9]+)/);
+    // If not a left padding element, just add before:m for each p.
+    if (detectedLeftPaddingScale === null) {
+      return `${s} ${ns} ${ns.replace(/p/, 'before:m')}`;
+    }
+    // each scale is worth 4px
+    // Need to subtract 2px from this for the before:-left-2px class...
+    // and add 2px for the padding-left
+    const padScale = Number.parseInt(detectedLeftPaddingScale[3]);
+    const sizePrefix = detectedLeftPaddingScale[1];
+    return `${s} ${sizePrefix}pl-[${
+      4 * padScale + 2
+    }px] ${sizePrefix}before:ml-[${4 * padScale - 2}px]`;
+  }, '');
+};
+
+type VariantPropTypes = VariantProps<typeof inputFieldStyles>;
+
+type FormProps = {
+  name: string;
+  type?: HTMLInputTypeAttribute;
+  placeholder?: string;
+  hidden?: boolean;
+};
+
+type FormFieldProps = FormProps &
+  VariantPropTypes &
+  Omit<HTMLProps<HTMLInputElement>, 'type'>;
+
+// This provides a wrapper for the TextField, providing the error behaviour.
+export const FormField: React.FC<FormFieldProps> = (props) => {
+  const { fullWidth, className: pCN, readOnly = false, ...inputProps } = props;
+  const id = props.name;
+  // if (readOnly) {
+  //   return <FormFieldIndicator {...props} />;
+  // }
+
+  const placeholderVisible = typeof props.placeholder !== 'undefined';
+
+  const labelPadding = calculateLabelPadding(pCN);
+  const borderMargin = calculateBorderMargin(pCN);
+
+  // placeholder-shown implies the field is empty
+  // focus should have the non-empty classNames
+
+  // IMPORTANT:
+  // The peer selectors require that the input is before the label in the DOM.
+  return (
+    <div
+      className={`${pCN ?? ''} ${wrapperStyles({
+        fullWidth,
+        hidden: props.hidden
+      })}`}
+    >
+      <input
+        id={id}
+        type={props.type ?? 'text'}
+        className={inputFieldStyles({
+          fullWidth,
+          readOnly: readOnly,
+          placeholderVisible
+        })}
+        placeholder={props.placeholder ?? id}
+        readOnly={readOnly}
+        {...inputProps}
+      />
+      <TopBorderElement paddingElems={borderMargin} />
+      {/* <div className='bg-inherit absolute h-2 -top-2 w-24 left-2 ' aria-hidden></div>
+      <div className='bg-white absolute h-2 top-0 w-24 left-2 ' aria-hidden></div> */}
+      <label
+        id={`${id}-label`}
+        htmlFor={id}
+        className={`${labelPadding} ${labelStyles({
+          placeholderVisible,
+          readOnly
+        })}`}
+      >
+        {props.label ?? id}
+      </label>
+    </div>
+  );
+};
+
+// Element to provide a top border when the label moves upwards
+const TopBorderElement = (props: { paddingElems?: string }) => {
+  const className = `${
+    props.paddingElems ?? ''
+  } absolute left-0 right-0 top-0 h-[1px] bg-gray-400 peer-focus:block`;
+  return <div className={className} aria-hidden />;
+};
---
+++ frontend/src/Components/Form/PersonSrv.tsx
@@ -0,0 +1,111 @@
+import { FieldValues } from 'react-hook-form';
+import { FormField } from './FormFieldSrv';
+
+export type PersonProps = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
+
+// Statically include these values
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const dummyClassNames = `
+xs:w-1/2 xs:pr-2 xs:pl-2 xs:mr-2 xs:ml-2 xs:before:ml-[6px] xs:pl-[10px] xs:flex-row
+sm:w-1/2 sm:pr-2 sm:pl-2 sm:mr-2 sm:ml-2 sm:before:ml-[6px] sm:pl-[10px] sm:flex-row
+md:w-1/2 md:pr-2 md:pl-2 md:mr-2 md:ml-2 md:before:ml-[6px] md:pl-[10px] md:flex-row
+lg:w-1/2 lg:pr-2 lg:pl-2 lg:mr-2 lg:ml-2 lg:before:ml-[6px] lg:pl-[10px] lg:flex-row
+xl:w-1/2 xl:pr-2 xl:pl-2 xl:mr-2 xl:ml-2 xl:before:ml-[6px] xl:pl-[10px] xl:flex-row
+`;
+
+type SharedProps = {
+  defaultValue?: PersonProps;
+  locked?: boolean;
+  heading?: string;
+  splitSize?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | null;
+};
+
+type PropsInNestedPerson<KeyName, FV extends FieldValues> = {
+  path: KeyName;
+} & SharedProps;
+
+type PropsInStandalonePerson = {
+  path?: undefined;
+} & SharedProps;
+
+/* prettier-ignore */
+type PersonTypeProps<FV extends FieldValues, K = keyof FV> =
+  FV extends PersonProps
+    ? PropsInStandalonePerson
+    : K extends keyof FV
+      ? FV[K] extends PersonProps
+        ? PropsInNestedPerson<K, FV>
+        : never
+      : never
+
+export function Person<FV extends FieldValues>(props: PersonTypeProps<FV>) {
+  const { defaultValue, locked, heading } = props;
+
+  const path =
+    typeof props.path !== 'undefined' ? `${String(props.path)}.` : '';
+  // Default to 'md' if not specified. Allow null to prevent splitting regardless of size
+  const splitSize =
+    typeof props.splitSize !== 'undefined' ? props.splitSize : 'md';
+
+  let headElem = null;
+  if (typeof heading !== 'undefined') {
+    headElem = (
+      <div className='mb-2 px-2'>
+        <span className='prose prose-sm'>{heading}</span>
+      </div>
+    );
+  }
+
+  const labels = {
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    email: 'Email'
+  };
+
+  const fieldProps = (field: keyof PersonProps) => {
+    return {
+      defaultValue: defaultValue?.[field],
+      label: labels[field],
+      readOnly: locked
+    };
+  };
+
+  return (
+    <div>
+      {headElem}
+      <div className={`flex flex-col ${splitSize}:flex-row`}>
+        <FormField
+          name={`${path}firstName`}
+          required={true}
+          maxLength={80}
+          className={`w-full px-0 ${splitSize}:w-1/2 ${splitSize}:pr-2`}
+          {...fieldProps('firstName')}
+        />
+        <FormField
+          name={`${path}lastName`}
+          required={true}
+          maxLength={100}
+          className={`w-full px-0 ${splitSize}:w-1/2 ${splitSize}:pl-2`}
+          {...fieldProps('lastName')}
+        />
+      </div>
+      <div>
+        <FormField
+          name={`${path}email`}
+          required={true}
+          pattern={'^S+@S+.S+$'}
+          fullWidth
+          {...fieldProps('email')}
+        />
+      </div>
+    </div>
+  );
+}
+
+export type EmailProps = {
+  email: string;
+};
