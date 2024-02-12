9b8b335ddc04334a189e658a992d2ad5cbbcd58e
Christian Butcher <chrisb2244@gmail.com>
2024-02-12 20:30:11 +0900

    Remove unused presentation_submissions api route.

--- frontend/src/app/api/presentation_submissions/route.ts
+++
@@ -1,7 +0,0 @@
-import { submissionsForYear } from '@/lib/databaseModels';
-import { redirect } from 'next/navigation';
-
-export function GET() {
-  // Default to 'this' year
-  redirect('/api/presentation_submissions/' + submissionsForYear);
-}
