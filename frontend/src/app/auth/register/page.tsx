9abb3988ebd01a385c1063c4911cd7847ec31e57
Christian Butcher <chrisb2244@gmail.com>
2024-03-02 11:05:02 +0900

    Add redirect handling for the login and registration forms.

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -2,15 +2,25 @@ import { createServerComponentClient } from '@/lib/supabaseServer';
 import { redirect } from 'next/navigation';
 import { RegistrationForm } from '../RegistrationForm';
 
-const RegistrationPage = async () => {
+const RegistrationPage = async ({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  const redirectToParam = searchParams?.redirectTo;
+  const redirectTo =
+    typeof redirectToParam === 'string'
+      ? decodeURI(redirectToParam)
+      : undefined;
+
   const supabase = createServerComponentClient();
   const session = (await supabase.auth.getSession()).data.session;
   const isLoggedIn = session !== null;
   if (isLoggedIn) {
-    redirect('/');
+    redirect(redirectTo ?? '/');
   }
 
-  return <RegistrationForm />;
+  return <RegistrationForm redirectTo={redirectTo} />;
 };
 
 export default RegistrationPage;
