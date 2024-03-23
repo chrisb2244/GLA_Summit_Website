91876acd4a80e80c28713fb5883232c23882e702
Christian Butcher <chrisb2244@gmail.com>
2024-03-23 18:59:55 +0900

    Fix centering on mobile for footer link.

--- frontend/src/app/_rootElements/Footer.tsx
+++ frontend/src/app/_rootElements/Footer.tsx
@@ -9,12 +9,8 @@ export const Footer: React.FC<React.PropsWithChildren<unknown>> = () => {
       <div className='flex min-h-[64px] items-center justify-between bg-primaryc px-6 text-white'>
         <div className='mx-auto flex flex-grow flex-col justify-between py-4 text-sm md:flex-row md:py-0'>
           <SocialMediaIcons />
-          <div className='flex'>
-            <Link
-              prefetch={false}
-              href='/our-team'
-              className='self-center py-[0.8px] underline md:py-0'
-            >
+          <div className='flex self-center py-[0.8px] underline md:py-0'>
+            <Link prefetch={false} href='/our-team' className='self-center'>
               About Us
             </Link>
           </div>
