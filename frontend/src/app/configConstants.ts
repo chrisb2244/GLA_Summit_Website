81ab04b21fa873490d012db1fd459559b9391f3b
Christian Butcher <chrisb2244@gmail.com>
2024-11-04 04:02:20 +0900

    Set the eventUrl in configConstants.ts.

--- frontend/src/app/configConstants.ts
+++ frontend/src/app/configConstants.ts
@@ -6,4 +6,5 @@ export const submissionsForYear: SummitYear = '2025';
 export const ticketYear: SummitYear = '2025';
 
 export const startDate = new Date(Date.UTC(2025, 2, 24, 12, 0, 0));
-export const eventUrl = 'https://hopin.com/events/gla-summit-2025';
+export const eventUrl =
+  'https://app.events.ringcentral.com/events/gla-summit-2025/';
