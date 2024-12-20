b5a48c7c530c07e7d42a2da86c23f287c755b4cd
Christian Butcher <chrisb2244@gmail.com>
2024-12-20 15:10:44 +0900

    Update the PresentationSubmissionFormSchema to pass the otherPresenters as an array of strings (emails).

--- frontend/src/Components/Forms/PresentationSubmissionFormSchema.ts
+++ frontend/src/Components/Forms/PresentationSubmissionFormSchema.ts
@@ -18,42 +18,48 @@ const StaticPresentationFormSchema = z.object({
   title: z.string(),
   abstract: z.string(),
   learningPoints: z.string(),
-  presentationType: SubmittablePresentationTypeSchema
+  presentationType: SubmittablePresentationTypeSchema,
+  otherPresenters: z.array(z.string().email()).optional()
 });
 const StaticKeys = StaticPresentationFormSchema.shape;
 
 export const PresentationSubmissionFormSchema =
-  StaticPresentationFormSchema.catchall(z.string().email()).transform(
-    (input, ctx) => {
-      const otherPresenters = Object.entries(input)
-        .map(([key, value]) => {
-          if (/otherPresenters\.[0-9]+\.email/.test(key)) {
-            return value as string;
-          }
-          if (Object.keys(StaticKeys).includes(key)) {
-            return null;
-          }
-          ctx.addIssue({
-            code: 'unrecognized_keys',
-            keys: [key],
-            message: 'Unexpected key',
-            path: [key],
-            fatal: true
-          });
-        })
-        .filter((v) => v !== null) as string[];
-      return {
-        title: input.title,
-        abstract: input.abstract,
-        submitter: {
-          firstName: input['submitter.firstName'],
-          lastName: input['submitter.lastName'],
-          email: input['submitter.email']
-        },
-        learningPoints: input.learningPoints,
-        presentationType: input.presentationType,
-        isFinal: input.isFinal ?? false,
-        otherPresenters
-      };
-    }
-  );
+  StaticPresentationFormSchema.catchall(z.any()).transform((input, ctx) => {
+    const otherPresenters = Object.entries(input)
+      .map(([key, value]) => {
+        if (/otherPresenters\.[0-9]+\.email/.test(key)) {
+          return value as string;
+        }
+        if (Object.keys(StaticKeys).includes(key)) {
+          return null;
+        }
+        if (key === 'action') {
+          return null;
+        }
+        if (key.startsWith('$ACTION')) {
+          return null;
+        }
+        ctx.addIssue({
+          code: 'unrecognized_keys',
+          keys: [key],
+          message: 'Unexpected key',
+          path: [key],
+          fatal: true
+        });
+        return null;
+      })
+      .filter((v) => v !== null);
+    return {
+      title: input.title,
+      abstract: input.abstract,
+      submitter: {
+        firstName: input['submitter.firstName'],
+        lastName: input['submitter.lastName'],
+        email: input['submitter.email']
+      },
+      learningPoints: input.learningPoints,
+      presentationType: input.presentationType,
+      isFinal: input.isFinal ?? false,
+      otherPresenters
+    };
+  });
