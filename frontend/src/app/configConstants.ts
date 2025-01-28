0fb3b93c64a01d597e7d39a28b36d56c2f35965f
Christian Butcher <chrisb2244@users.noreply.github.com>
2025-01-28 23:17:51 +0900

    Update startDate

--- frontend/src/app/configConstants.ts
+++ frontend/src/app/configConstants.ts
@@ -5,6 +5,6 @@ export const currentDisplayYear: SummitYear = '2024';
 export const submissionsForYear: SummitYear = '2025';
 export const ticketYear: SummitYear = '2025';
 
-export const startDate = new Date(Date.UTC(2025, 2, 24, 12, 0, 0));
+export const startDate = new Date(Date.UTC(2025, 5, 23, 12, 0, 0));
 export const eventUrl =
   'https://app.events.ringcentral.com/events/gla-summit-2025/';
