8b55503583affe06caf593bd5037b009840fd2ee
Christian Butcher <chrisb2244@gmail.com>
2025-11-04 11:06:51 +0900

    Merge a number of older migrations into a new migration to add the function and table to track reviews by organizers of submissions.

--- supabase/migrations/20251102040150_initial_squashed_remote_changes.sql
+++ supabase/migrations/20251102040150_initial_squashed_remote_changes.sql
@@ -64,33 +64,6 @@ CREATE OR REPLACE FUNCTION "public"."get_presentation_ids"("p_id" "uuid") RETURN
 select array_agg(presentation_id) presentations from presentation_presenters where presenter_id=p_id group by presenter_id
 $$;
 
-CREATE OR REPLACE FUNCTION "public"."get_reviewable_submissions"("target_year" "public"."summit_year") RETURNS TABLE("presentation_id" "uuid", "title" "text", "abstract" "text", "presentation_type" "public"."presentation_type", "learning_points" "text", "submitter_id" "uuid", "presenters" "public"."presenter_info"[], "updated_at" timestamp with time zone)
-    LANGUAGE "plpgsql"
-    SET "search_path" TO 'public'
-    AS $$
-BEGIN
-  -- IF (SELECT count(*) FROM organizers WHERE id = auth.uid()) != 1 then
-  --   -- Not an organizer
-  --   RETURN;
-  -- END IF;
-
-  RETURN QUERY
-  SELECT 
-    ps.id,
-    ps.title,
-    ps.abstract,
-    ps.presentation_type,
-    ps.learning_points,
-    ps.submitter_id,
-    array_agg( row(p.id, p.firstname, p.lastname)::presenter_info ),
-    ps.updated_at
-  FROM presentation_submissions ps 
-    JOIN presentation_presenters pp ON pp.presentation_id = ps.id
-    JOIN profiles p ON p.id = pp.presenter_id
-  WHERE ps.year = target_year
-  GROUP BY ps.id;
-END; $$;
-
 SET default_tablespace = '';
 SET default_table_access_method = "heap";
 
@@ -350,33 +323,7 @@ CREATE OR REPLACE FUNCTION public.get_presentation_ids(p_id uuid)
 AS $function$
 select array_agg(presentation_id) presentations from presentation_presenters where presenter_id=p_id group by presenter_id
 $function$;
-CREATE OR REPLACE FUNCTION public.get_reviewable_submissions(target_year summit_year)
- RETURNS TABLE(presentation_id uuid, title text, abstract text, presentation_type presentation_type, learning_points text, submitter_id uuid, presenters presenter_info[], updated_at timestamp with time zone)
- LANGUAGE plpgsql
- SET search_path TO 'public'
-AS $function$
-BEGIN
-  -- IF (SELECT count(*) FROM organizers WHERE id = auth.uid()) != 1 then
-  --   -- Not an organizer
-  --   RETURN;
-  -- END IF;
 
-  RETURN QUERY
-  SELECT 
-    ps.id,
-    ps.title,
-    ps.abstract,
-    ps.presentation_type,
-    ps.learning_points,
-    ps.submitter_id,
-    array_agg( row(p.id, p.firstname, p.lastname)::presenter_info ),
-    ps.updated_at
-  FROM presentation_submissions ps 
-    JOIN presentation_presenters pp ON pp.presentation_id = ps.id
-    JOIN profiles p ON p.id = pp.presenter_id
-  WHERE ps.year = target_year
-  GROUP BY ps.id;
-END; $function$;
 CREATE OR REPLACE FUNCTION public.is_ok(presentation_presenters)
  RETURNS boolean
  LANGUAGE plpgsql
@@ -549,33 +496,6 @@ CREATE OR REPLACE FUNCTION public.get_presentation_ids(p_id uuid)
 AS $function$
 select array_agg(presentation_id) presentations from presentation_presenters where presenter_id=p_id group by presenter_id
 $function$;
-CREATE OR REPLACE FUNCTION public.get_reviewable_submissions(target_year summit_year)
- RETURNS TABLE(presentation_id uuid, title text, abstract text, presentation_type presentation_type, learning_points text, submitter_id uuid, presenters presenter_info[], updated_at timestamp with time zone)
- LANGUAGE plpgsql
- SET search_path TO 'public'
-AS $function$
-BEGIN
-  -- IF (SELECT count(*) FROM organizers WHERE id = auth.uid()) != 1 then
-  --   -- Not an organizer
-  --   RETURN;
-  -- END IF;
-
-  RETURN QUERY
-  SELECT 
-    ps.id,
-    ps.title,
-    ps.abstract,
-    ps.presentation_type,
-    ps.learning_points,
-    ps.submitter_id,
-    array_agg( row(p.id, p.firstname, p.lastname)::presenter_info ),
-    ps.updated_at
-  FROM presentation_submissions ps 
-    JOIN presentation_presenters pp ON pp.presentation_id = ps.id
-    JOIN profiles p ON p.id = pp.presenter_id
-  WHERE ps.year = target_year
-  GROUP BY ps.id;
-END; $function$;
 
 CREATE OR REPLACE FUNCTION public.is_ok(presentation_presenters)
  RETURNS boolean
