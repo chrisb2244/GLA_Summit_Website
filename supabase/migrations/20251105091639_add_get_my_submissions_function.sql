d92e1c61379031cfee8c91c1adb91f998357dc79
Christian Butcher <chrisb2244@gmail.com>
2025-11-05 18:35:08 +0900

    Refactor the get_my_submissions function into a new migration. The 'all_emails' output was removed to avoid a potential surface to secure (it was previously used for editable presentation drafts, to update copresenters, but this can be reworked if reintroduced). Now fully migrated squashed SQL files are removed, completing the migration to new files.

---
+++ supabase/migrations/20251105091639_add_get_my_submissions_function.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE FUNCTION public.get_my_submissions()
+  RETURNS TABLE(
+    presentation_id uuid,
+    title text,
+    abstract text,
+    learning_points text,
+    presentation_type presentation_type,
+    submitter_id uuid,
+    is_submitted boolean,
+    year summit_year,
+    all_presenters_ids uuid[],
+    all_firstnames text[],
+    all_lastnames text[]
+  )
+  LANGUAGE sql
+  SECURITY DEFINER
+  SET search_path TO ''
+  AS $function$
+    SELECT
+      ps.id as presentation_id,
+      ps.title,
+      ps.abstract,
+      ps.learning_points,
+      ps.presentation_type,
+      ps.submitter_id,
+      ps.is_submitted,
+      ps.year,
+      array_agg(ppn.presenter_id) as all_presenters,
+      array_agg(ppn.firstname) as all_firstnames,
+      array_agg(ppn.lastname) as all_lastnames
+    FROM public.presentation_submissions ps
+    JOIN (
+      SELECT
+        pp.presentation_id,
+        pp.presenter_id,
+        prof.firstname,
+        prof.lastname
+      FROM public.presentation_presenters pp
+      LEFT JOIN public.profiles prof
+        ON pp.presenter_id = prof.id
+    ) ppn on ps.id = ppn.presentation_id
+    WHERE presentation_id IN (
+      SELECT presentation_id
+      FROM public.presentation_presenters ppp
+      WHERE ppp.presenter_id = auth.uid()
+    )
+    GROUP BY ps.id;
+  $function$;
+
+REVOKE ALL ON FUNCTION public.get_my_submissions FROM public;
+REVOKE ALL ON FUNCTION public.get_my_submissions FROM anon;
