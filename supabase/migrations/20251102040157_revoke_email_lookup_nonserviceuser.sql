9c90033259f4ce147672bf8592f2efa7aff4be8b
Christian Butcher <chrisb2244@gmail.com>
2025-11-05 18:35:08 +0900

    Refactor the get_my_submissions function into a new migration. The 'all_emails' output was removed to avoid a potential surface to secure (it was previously used for editable presentation drafts, to update copresenters, but this can be reworked if reintroduced). Now fully migrated squashed SQL files are removed, completing the migration to new files.

--- supabase/migrations/20251102040157_revoke_email_lookup_nonserviceuser.sql
+++
@@ -1,3 +0,0 @@
-REVOKE ALL ON FUNCTION "public"."get_email_by_id"("user_id" "uuid") FROM "public";
-REVOKE ALL ON FUNCTION "public"."get_email_by_id"("user_id" "uuid") FROM "anon";
-REVOKE ALL ON FUNCTION "public"."get_email_by_id"("user_id" "uuid") FROM "authenticated";
