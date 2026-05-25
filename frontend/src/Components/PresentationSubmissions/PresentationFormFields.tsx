3f6eb4e3edc31f1ddb0b37f7f19fd78912b0dbc0
Christian Butcher <chrisb2244@gmail.com>
2026-05-26 01:26:18 +0900

    Reimplement the button sizing choices for the Presentation Form without breaking other Button usage.

--- frontend/src/Components/PresentationSubmissions/PresentationFormFields.tsx
+++ frontend/src/Components/PresentationSubmissions/PresentationFormFields.tsx
@@ -347,21 +347,30 @@ export const PresentationFormFields = ({
 
       <div className='mt-3 flex flex-col gap-2 sm:flex-row sm:justify-between'>
         {CAN_SUBMIT_DRAFT && (
-          <Button type='submit' name='submitIntent' value='saveDraft'>
-            Save Draft
-          </Button>
+          <div className='w-2/5'>
+            <Button
+              type='submit'
+              name='submitIntent'
+              value='saveDraft'
+              fullWidth
+            >
+              Save Draft
+            </Button>
+          </div>
         )}
 
-        <SubmitButton
-          name='submitIntent'
-          value='submit'
-          staticText={
-            state.duplicateWarning ? 'Submit Anyway' : 'Submit Presentation'
-          }
-          pendingText={
-            state.duplicateWarning ? 'Submitting anyway...' : 'Submitting...'
-          }
-        />
+        <div className={CAN_SUBMIT_DRAFT ? 'w-3/5' : 'w-full'}>
+          <SubmitButton
+            name='submitIntent'
+            value='submit'
+            staticText={
+              state.duplicateWarning ? 'Submit Anyway' : 'Submit Presentation'
+            }
+            pendingText={
+              state.duplicateWarning ? 'Submitting anyway...' : 'Submitting...'
+            }
+          />
+        </div>
       </div>
     </form>
   );
