7de6a3c5ef270ef823ea6dc9715c5fcf2edf3e69
Christian Butcher <chrisb2244@gmail.com>
2026-08-31 15:40:17 +0100

    Restore the agenda in the navigation, and redirect /agenda.

--- frontend/src/app/presentation-list/layout.tsx
+++ frontend/src/app/presentation-list/layout.tsx
@@ -1,6 +1,6 @@
 import { PageIntro } from '@/Components/Typography';
 import { YearNav } from '@/Components/YearNav';
-// import Link from 'next/link';
+import Link from 'next/link';
 import React from 'react';
 
 const PresentationListLayout = async ({
@@ -8,17 +8,15 @@ const PresentationListLayout = async ({
 }: {
   children: React.ReactNode;
 }) => {
-  // const agendaElem = (
-  //   <>
-  //     <p>
-  //       For a list by schedule, see our{' '}
-  //       <Link href='/full-agenda' className='link'>
-  //         agenda
-  //       </Link>
-  //       .
-  //     </p>
-  //   </>
-  // );
+  const agendaElem = (
+    <p className='mx-4 mb-2'>
+      For a list by schedule, see our{' '}
+      <Link href='/full-agenda' className='link'>
+        agenda
+      </Link>
+      .
+    </p>
+  );
 
   // const agendaElem = (
   //   <p>
@@ -33,7 +31,7 @@ const PresentationListLayout = async ({
         Presentations below are grouped by year, and sorted by the first
         speaker&apos;s name.
       </PageIntro>
-      {/* {agendaElem} */}
+      {agendaElem}
 
       <YearNav basePath='/presentation-list' />
       <div className='shadow'>{children}</div>
